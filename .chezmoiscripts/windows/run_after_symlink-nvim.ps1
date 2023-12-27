ce7d1d32c069949d8d73c8ee0f2e538c104dfce9
Nitestack <74626967+Nitestack@users.noreply.github.com>
2023-12-27 23:12:51 +0100

    feat: add log message to symlinking

--- .chezmoiscripts/windows/run_after_symlink-nvim.ps1
+++ .chezmoiscripts/windows/run_after_symlink-nvim.ps1
@@ -1,4 +1,5 @@
 if (Test-Path "$env:USERPROFILE\.config\nvim")
 {
+  Write-Host "Linking $env:LOCALAPPDATA\nvim to $env:USERPROFILE\.config\nvim"
   New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.config\nvim"
 }
