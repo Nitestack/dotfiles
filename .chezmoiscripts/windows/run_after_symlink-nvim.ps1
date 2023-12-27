146f0b2d6f606166ed461c39bf9cc0bfb35c17f6
Nitestack <74626967+Nitestack@users.noreply.github.com>
2023-12-27 23:24:41 +0100

    fix: only create symlink if it doesn't exist

--- .chezmoiscripts/windows/run_after_symlink-nvim.ps1
+++ .chezmoiscripts/windows/run_after_symlink-nvim.ps1
@@ -1,4 +1,4 @@
-if (Test-Path "$env:USERPROFILE\.config\nvim")
+if (!(Test-Path("$env:LOCALAPPDATA\nvim")) -or !((Get-Item "$env:LOCALAPPDATA\nvim").Attributes.ToString() -match "ReparsePoint"))
 {
   Write-Host "Linking $env:LOCALAPPDATA\nvim to $env:USERPROFILE\.config\nvim"
   New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.config\nvim"
