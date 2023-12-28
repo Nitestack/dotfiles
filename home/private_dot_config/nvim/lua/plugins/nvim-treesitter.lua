ebae3be869c1494f4a3792f4598053e194052145
Nitestack <74626967+Nitestack@users.noreply.github.com>
2023-12-28 14:15:48 +0100

    update(nvim): added chezmoi plugins

--- home/private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -11,29 +11,29 @@ return {
       vim.list_extend(opts.ensure_installed, core.config.plugins.treesitter)
     end
   end,
-  ---@param opts TSConfig
-  config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      ---@type table<string, boolean>
-      local added = {}
-      opts.ensure_installed = vim.tbl_filter(function(lang)
-        if added[lang] then
-          return false
-        end
-        added[lang] = true
-        return true
-      end, opts.ensure_installed)
-    end
-    require("nvim-treesitter.configs").setup(opts)
-
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.gotmpl = {
-      install_info = {
-        url = "https://github.com/ngalaiko/tree-sitter-go-template",
-        files = { "src/parser.c" },
-      },
-      filetype = "gotmpl",
-      used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
-    }
-  end,
+  -- ---@param opts TSConfig
+  -- config = function(_, opts)
+  --   if type(opts.ensure_installed) == "table" then
+  --     ---@type table<string, boolean>
+  --     local added = {}
+  --     opts.ensure_installed = vim.tbl_filter(function(lang)
+  --       if added[lang] then
+  --         return false
+  --       end
+  --       added[lang] = true
+  --       return true
+  --     end, opts.ensure_installed)
+  --   end
+  --   require("nvim-treesitter.configs").setup(opts)
+  --
+  --   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  --   parser_config.gotmpl = {
+  --     install_info = {
+  --       url = "https://github.com/ngalaiko/tree-sitter-go-template",
+  --       files = { "src/parser.c" },
+  --     },
+  --     filetype = "gotmpl",
+  --     used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+  --   }
+  -- end,
 }
