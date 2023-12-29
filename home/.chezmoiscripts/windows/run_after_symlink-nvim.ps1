6b6e0de777d7045b65ab53848697255ef0a0fd86
Nitestack <74626967+Nitestack@users.noreply.github.com>
2023-12-29 02:12:07 +0100

    feat: add infos about script running

--- home/.chezmoiscripts/windows/run_after_symlink-nvim.ps1
+++ home/.chezmoiscripts/windows/run_after_symlink-nvim.ps1
@@ -1,5 +1,16 @@
-if (!(Test-Path("$env:LOCALAPPDATA\nvim")) -or !((Get-Item "$env:LOCALAPPDATA\nvim").Attributes.ToString() -match "ReparsePoint"))
+Write-Host "nvim: Setting up Neovim configuration..."
+
+$symlink_path = "$env:LOCALAPPDATA\nvim"
+
+if (!(Test-Path("$symlink_path")) -or !((Get-Item "$symlink_path").Attributes.ToString() -match "ReparsePoint"))
 {
-  Write-Host "Linking $env:LOCALAPPDATA\nvim to $env:USERPROFILE\.config\nvim"
-  New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.config\nvim"
+  $target_path = "$env:USERPROFILE\.config\nvim"
+
+  New-Item -Path "$symlink_path" -ItemType SymbolicLink -Value "$target_path"
+
+  Write-Host "      Symbolic link for Neovim configuration created."
+  Write-Host "      $symlink_path -> $target_path"
+} 
+else {
+  Write-Host "      Symbolic link for Neovim configuration already exists. Skipping."
 }
