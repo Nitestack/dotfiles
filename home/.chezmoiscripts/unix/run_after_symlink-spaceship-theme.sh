73459e7c50a97a7e3619b71a6979217b227eb7bd
Nitestack <74626967+Nitestack@users.noreply.github.com>
2023-12-29 13:28:54 +0100

    feat: improve scripts

--- home/.chezmoiscripts/unix/run_after_symlink-spaceship-theme.sh
+++ home/.chezmoiscripts/unix/run_after_symlink-spaceship-theme.sh
@@ -3,16 +3,22 @@
 echo "zsh: Setting up Spaceship theme..."
 
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+target_path="$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 symlink_path="$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme"
 
-# Check if the symbolic link doesn't exist
+# Check if the path exists
 if [[ ! -e "$symlink_path" ]]; then
-  target_path="$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-  ln -s "$symlink_path" "$target_path"
-
-  echo "     Symbolic link for Spaceship theme created."
-  echo "     $symlink_path -> $target_path"
-else
-  echo "     Symbolic link for Spaceship theme already exists. Skipping."
+  echo "     Spaceship theme not found. Skipping."
+  return
 fi
+
+# Check if the symbolic link already exists
+if [[ -L "$target_path" ]]; then
+  echo "     Symbolic link for Spaceship theme already exists. Skipping."
+  return
+fi
+
+ln -s "$symlink_path" "$target_path"
+
+echo "     Symbolic link for Spaceship theme created."
+echo "     $symlink_path -> $target_path"
--- home/.chezmoiscripts/windows/run_after_symlink-nvim.ps1
+++ home/.chezmoiscripts/windows/run_after_symlink-nvim.ps1
@@ -1,16 +1,23 @@
 Write-Host "nvim: Setting up Neovim configuration..."
 
-$symlink_path = "$env:LOCALAPPDATA\nvim"
+$target_path = "$env:LOCALAPPDATA\nvim"
+$symlink_path = "$env:USERPROFILE\.config\nvim"
 
-if (!(Test-Path("$symlink_path")) -or !((Get-Item "$symlink_path").Attributes.ToString() -match "ReparsePoint"))
+# Check if the path exists
+if (!(Test-Path("$symlink_path")))
 {
-  $target_path = "$env:USERPROFILE\.config\nvim"
-
-  New-Item -Path "$symlink_path" -ItemType SymbolicLink -Value "$target_path"
-
-  Write-Host "      Symbolic link for Neovim configuration created."
-  Write-Host "      $symlink_path -> $target_path"
-} 
-else {
-  Write-Host "      Symbolic link for Neovim configuration already exists. Skipping."
+  Write-Host "      Neovim configuration not found. Skipping."
+  return
 }
+
+# Check if the symbolic link already exists
+if (Test-Path("$target_path"))
+{
+  Write-Host "      Symbolic link for Neovim configuration already exists. Skipping."
+  return
+}
+
+New-Item -Path "$symlink_path" -ItemType SymbolicLink -Value "$target_path"
+
+Write-Host "      Symbolic link for Neovim configuration created."
+Write-Host "      $symlink_path -> $target_path"
