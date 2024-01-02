4e7fe513407ef6f98bf8904d111df6b2383a0c9e
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-02 19:15:04 +0100

    feat(nvim): add cloak to hide env variables

---
+++ home/private_dot_config/nvim/lua/plugins/cloak.lua
@@ -0,0 +1,16 @@
+---@type LazyPluginSpec
+return {
+  "laytan/cloak.nvim",
+  event = "LazyFile",
+  cmd = { "CloakEnable", "CloakDisable", "CloakToggle" },
+  keys = core.lazy_map({
+    n = {
+      ["<leader>ct"] = {
+        function()
+          require("cloak").toggle()
+        end,
+      },
+    },
+  }),
+  opts = {},
+}
