63a5a58146e6005ed34bd7c6ab7143be5ff99fd2
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-04 02:37:51 +0100

    feat(nvim): add vim-matchup for extended text objects

--- home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-treesitter.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-treesitter.lua
@@ -1,6 +1,9 @@
 ---@type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "andymasson/vim-matchup",
+  },
   event = function()
     return {
       "LazyFile",
@@ -10,5 +13,10 @@ return {
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, core.config.plugins.treesitter)
     end
+
+    opts.matchup = {
+      enable = true,
+      disable_virtual_text = true,
+    }
   end,
 }
