bee3858cc2baac151e0338f2f218a9817d2d7177
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-04 01:46:09 +0100

    feat(nvim): add filetypes

--- home/private_dot_config/exact_nvim/lua/core/filetypes.lua
+++ home/private_dot_config/exact_nvim/lua/core/filetypes.lua
@@ -5,13 +5,19 @@
 ---@type vim.filetype.add.filetypes
 local M = {}
 
+M.extension = {
+  mdx = "markdown",
+}
+
 M.filename = {
   [".eslintrc.json"] = "jsonc",
+  [".npmignore"] = "ignore",
 }
 
 M.pattern = {
   ["tsconfig*.json"] = "jsonc",
   [".*/%.vscode/.*%.json"] = "jsonc",
+  ["tsconfig.tsbuildinfo"] = "json",
 }
 
 return M
