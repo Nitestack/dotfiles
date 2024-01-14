e93912c0bb17133ecf13e1bf625181a71aba7f5d
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-14 13:18:06 +0100

    feat(nvim): update toggleterm

--- home/private_dot_config/exact_nvim/lua/exact_plugins/toggleterm.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/toggleterm.lua
@@ -7,17 +7,28 @@ return {
       ["<C-t>"] = {},
     },
   }),
-  ---@type ToggleTermConfig
-  opts = {
-    open_mapping = "<C-t>",
-    direction = "float",
-    highlights = {
-      NormalFloat = {
-        link = "NormalFloat",
-      },
-    },
-    float_opts = {
-      border = core.config.ui.transparent.floats and "curved" or "none",
-    },
-  },
+  ---@param opts ToggleTermConfig
+  opts = function(_, opts)
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    opts.size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end
+
+    opts.open_mapping = "<C-t>"
+    opts.direction = "float"
+
+    opts.highlights = opts.highlights or {}
+    opts.highlights.NormalFloat = {
+      link = "NormalFloat",
+    }
+
+    opts.float_opts = opts.float_opts or {}
+    opts.float_opts.border = core.config.ui.transparent.floats and "curved" or "none"
+    opts.width = math.floor(core.config.ui.width * vim.fn.winwidth(0))
+    opts.height = math.floor(core.config.ui.height * vim.fn.winheight(0))
+  end,
 }
