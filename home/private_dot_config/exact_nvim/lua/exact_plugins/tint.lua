610fe3fa029d55e8e6c3e038d08757d45c76efc7
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-14 13:18:21 +0100

    feat(nvim): tint inactive windows

---
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/tint.lua
@@ -0,0 +1,13 @@
+---@type LazyPluginSpec
+return {
+  "levouh/tint.nvim",
+  event = "VeryLazy",
+  opts = {
+    tint = -80,
+    highlight_ignore_patterns = {
+      "SymbolUsage*",
+      "LineNr",
+      "CursorLineNr",
+    },
+  },
+}
