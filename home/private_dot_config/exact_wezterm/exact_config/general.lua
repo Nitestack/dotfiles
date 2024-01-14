23b1c17943ba7d9a5a7cc4f90bbfc7b120cca48b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-15 00:12:26 +0100

    feat(wezterm): add link highlight rules

--- home/private_dot_config/exact_wezterm/exact_config/general.lua
+++ home/private_dot_config/exact_wezterm/exact_config/general.lua
@@ -1,3 +1,38 @@
 return {
-  window_close_confirmation = "NeverPrompt",
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = "\\((\\w+://\\S+)\\)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = "\\[(\\w+://\\S+)\\]",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = "\\{(\\w+://\\S+)\\}",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = "<(\\w+://\\S+)>",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      regex = "\\b\\w+://\\S+[)/a-zA-Z0-9-]+",
+      format = "$0",
+    },
+    -- implicit mailto link
+    {
+      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+      format = "mailto:$0",
+    },
+  },
 }
