c1ef59cb0a7b83faeb2e442b492703702e026d79
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-14 12:12:27 +0100

    fix(nvim): remove enter closing

--- home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-autopairs.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-autopairs.lua
@@ -1,42 +1,3 @@
-local get_closing_for_line = function(line)
-  local i = -1
-  local clo = ""
-
-  while true do
-    i, _ = string.find(line, "[%(%)%{%}%[%]]", i + 1)
-    if i == nil then
-      break
-    end
-    local ch = string.sub(line, i, i)
-    local st = string.sub(clo, 1, 1)
-
-    if ch == "{" then
-      clo = "}" .. clo
-    elseif ch == "}" then
-      if st ~= "}" then
-        return ""
-      end
-      clo = string.sub(clo, 2)
-    elseif ch == "(" then
-      clo = ")" .. clo
-    elseif ch == ")" then
-      if st ~= ")" then
-        return ""
-      end
-      clo = string.sub(clo, 2)
-    elseif ch == "[" then
-      clo = "]" .. clo
-    elseif ch == "]" then
-      if st ~= "]" then
-        return ""
-      end
-      clo = string.sub(clo, 2)
-    end
-  end
-
-  return clo
-end
-
 ---@type LazyPluginSpec
 return {
   "windwp/nvim-autopairs",
@@ -117,20 +78,5 @@ return {
       "javascript",
       "javascriptreact",
     }):use_regex(true):set_end_pair_length(2))
-
-    -- Expand multiple pairs on enter key
-    autopairs.remove_rule("(")
-    autopairs.remove_rule("{")
-    autopairs.remove_rule("[")
-
-    autopairs.add_rule(Rule("[%(%{%[]", "")
-      :use_regex(true)
-      :replace_endpair(function(o)
-        return get_closing_for_line(o.line)
-      end)
-      :end_wise(function(o)
-        -- Do not endwise if there is no closing
-        return get_closing_for_line(o.line) ~= ""
-      end))
   end,
 }
