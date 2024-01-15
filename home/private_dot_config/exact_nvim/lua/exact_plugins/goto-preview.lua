3a1bb20069a9e8aa53ddd2bd9cf3b7c2b2f31156
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-15 14:14:51 +0100

    feat(nvim): lazy-load mappings

--- home/private_dot_config/exact_nvim/lua/exact_plugins/goto-preview.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/goto-preview.lua
@@ -1,8 +1,48 @@
+---@diagnostic disable: missing-parameter
 ---@type LazyPluginSpec
 return {
   "rmagatti/goto-preview",
-  event = "LazyFile",
+  keys = core.lazy_map({
+    n = {
+      ["gpd"] = {
+        function()
+          require("goto-preview").goto_preview_definition()
+        end,
+        "Preview: Goto Definition",
+      },
+      ["gpt"] = {
+        function()
+          require("goto-preview").goto_preview_type_definition()
+        end,
+        "Preview: Goto Type Definition",
+      },
+      ["gpi"] = {
+        function()
+          require("goto-preview").goto_preview_implementation()
+        end,
+        "Preview: Goto Implementation",
+      },
+      ["gpD"] = {
+        function()
+          require("goto-preview").goto_preview_declaration()
+        end,
+        "Preview: Goto Declaration",
+      },
+      ["gP"] = {
+        function()
+          require("goto-preview").close_all_win()
+        end,
+        "Preview: Close Preview",
+      },
+      ["gpr"] = {
+        function()
+          require("goto-preview").goto_preview_references()
+        end,
+        "Preview: Goto References",
+      },
+    },
+  }),
   opts = {
-    default_mappings = true,
+    default_mappings = false,
   },
 }
