834d67d1be961ee21bf406882fb1b431bfb1b64a
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-15 15:01:13 +0100

    feat(nvim): add which-key prefix

--- home/private_dot_config/exact_nvim/lua/exact_plugins/goto-preview.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/goto-preview.lua
@@ -1,60 +1,72 @@
 ---@diagnostic disable: missing-parameter
----@type LazyPluginSpec
+---@type LazySpec
 return {
-  "rmagatti/goto-preview",
-  keys = core.lazy_map({
-    n = {
-      ["gpd"] = {
-        function()
-          require("goto-preview").goto_preview_definition()
-        end,
-        "Preview: Goto Definition",
+  {
+    "rmagatti/goto-preview",
+    keys = core.lazy_map({
+      n = {
+        ["gpd"] = {
+          function()
+            require("goto-preview").goto_preview_definition()
+          end,
+          "Preview: Goto Definition",
+        },
+        ["gpt"] = {
+          function()
+            require("goto-preview").goto_preview_type_definition()
+          end,
+          "Preview: Goto Type Definition",
+        },
+        ["gpi"] = {
+          function()
+            require("goto-preview").goto_preview_implementation()
+          end,
+          "Preview: Goto Implementation",
+        },
+        ["gpD"] = {
+          function()
+            require("goto-preview").goto_preview_declaration()
+          end,
+          "Preview: Goto Declaration",
+        },
+        ["gP"] = {
+          function()
+            require("goto-preview").close_all_win()
+          end,
+          "Preview: Close all windows",
+        },
+        ["gpr"] = {
+          function()
+            require("goto-preview").goto_preview_references()
+          end,
+          "Preview: Goto References",
+        },
       },
-      ["gpt"] = {
-        function()
-          require("goto-preview").goto_preview_type_definition()
-        end,
-        "Preview: Goto Type Definition",
-      },
-      ["gpi"] = {
-        function()
-          require("goto-preview").goto_preview_implementation()
-        end,
-        "Preview: Goto Implementation",
-      },
-      ["gpD"] = {
-        function()
-          require("goto-preview").goto_preview_declaration()
-        end,
-        "Preview: Goto Declaration",
-      },
-      ["gP"] = {
-        function()
-          require("goto-preview").close_all_win()
-        end,
-        "Preview: Close Preview",
-      },
-      ["gpr"] = {
-        function()
-          require("goto-preview").goto_preview_references()
-        end,
-        "Preview: Goto References",
+    }),
+    opts = function(_, opts)
+      opts.default_mappings = false
+      opts.border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+      opts.post_open_hook = function(bufnr)
+        core.single_map("n", "q", {
+          vim.cmd.q,
+          "Preview: Close Preview",
+          opts = {
+            noremap = true,
+            silent = true,
+            buffer = bufnr,
+          },
+        })
+      end
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["gp"] = {
+          name = "+goto preview",
+        },
       },
     },
-  }),
-  opts = function(_, opts)
-    opts.default_mappings = false
-    opts.border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
-    opts.post_open_hook = function(bufnr)
-      core.single_map("n", "q", {
-        vim.cmd.q,
-        "Preview: Close Preview",
-        opts = {
-          noremap = true,
-          silent = true,
-          buffer = bufnr,
-        },
-      })
-    end
-  end,
+  },
 }
