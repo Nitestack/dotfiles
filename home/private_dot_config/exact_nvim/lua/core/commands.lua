a09e915ed484f4b4caef4476af4883efd50059e1
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-17 00:16:59 +0100

    feat(nvim): remove unnecessary hide cursor

--- home/private_dot_config/exact_nvim/lua/core/commands.lua
+++ home/private_dot_config/exact_nvim/lua/core/commands.lua
@@ -16,31 +16,6 @@ local M = {}
 --  Autocommands
 --------------------------------------------------------------------------------
 M.auto_cmds = {
-  -- show cursor line only in active window
-  {
-    { "InsertLeave", "WinEnter" },
-    {
-      callback = function()
-        local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-        if ok and cl then
-          vim.wo.cursorline = true
-          vim.api.nvim_win_del_var(0, "auto-cursorline")
-        end
-      end,
-    },
-  },
-  {
-    { "InsertEnter", "WinLeave" },
-    {
-      callback = function()
-        local cl = vim.wo.cursorline
-        if cl then
-          vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
-          vim.wo.cursorline = false
-        end
-      end,
-    },
-  },
   -- Close some filetypes with <q>
   {
     "FileType",
