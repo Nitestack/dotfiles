ecf7fd4f142db8cfc6db3c76137f3a509e573b57
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-20 02:03:32 +0100

    feat(nvim): enhance cmp ui

--- home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-cmp.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-cmp.lua
@@ -3,24 +3,21 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      {
-        "roobert/tailwindcss-colorizer-cmp.nvim",
-        opts = {},
-      },
+      "roobert/tailwindcss-colorizer-cmp.nvim",
       "onsails/lspkind.nvim",
+      "L3MON4D3/LuaSnip",
+      "windwp/nvim-autopairs",
       -- Sources
       "hrsh7th/cmp-emoji",
-      -- Snippets
-      "L3MON4D3/LuaSnip",
-      -- Auto pairs
-      "windwp/nvim-autopairs",
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       local lspkind = require("lspkind")
+      local tailwindcss_colorizer = require("tailwindcss-colorizer-cmp")
 
+      tailwindcss_colorizer.setup()
       lspkind.init()
 
       local max_menu_width = math.min(50, math.floor(vim.o.columns * 0.5))
@@ -33,10 +30,21 @@ return {
 
       -- Window
       opts.window = opts.window or {}
-      opts.window.completion = cmp.config.window.bordered()
+      opts.window.completion = cmp.config.window.bordered({
+        scrollbar = false,
+        scrolloff = 8,
+        col_offset = -3,
+      })
       opts.window.documentation = cmp.config.window.bordered()
 
-      -- Formatting (VSCode-like completion window)
+      -- Formatting
+      local duplicates = {
+        buffer = 1,
+        path = 1,
+        nvim_lsp = 0,
+        luasnip = 1,
+      }
+      local duplicates_default = 0
       opts.formatting = opts.formatting or {}
       opts.formatting.fields = {
         "kind",
@@ -62,6 +70,9 @@ return {
         show_menu_labelDetails = true,
         before = function(entry, vim_item)
           vim_item = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
+          if vim.tbl_contains({ "nvim_lsp" }, entry.source.name) then
+            vim_item.dup = duplicates[entry.source.name] or duplicates_default
+          end
           return vim_item
         end,
       })
@@ -69,7 +80,6 @@ return {
       -- Experimental
       opts.experimental = opts.experimental or {}
       opts.experimental.ghost_text = false
-      opts.experimental.native_menu = false
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
