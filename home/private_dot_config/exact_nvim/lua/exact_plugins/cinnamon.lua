41551d894aa79812bd1912a1e905b2a7c2c3ce85
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-23 14:53:59 +0100

    feat(nvim): disable overriding keymaps

--- home/private_dot_config/exact_nvim/lua/exact_plugins/cinnamon.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/cinnamon.lua
@@ -6,10 +6,11 @@ return {
     default_keymaps = true,
     extra_keymaps = true,
     extended_keymaps = true,
-    override_keymaps = true,
+    override_keymaps = false,
 
     default_delay = 1,
     hide_cursor = true,
+    horizonal_scroll = false,
     scroll_limit = 50,
   },
 }
