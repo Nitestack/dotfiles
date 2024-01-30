9d74d6fd43eb494200a53f9d653f0f5d575d8864
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-01-30 23:13:27 +0100

    feat(nvim): remove dressing as provider for inc-rename

--- home/private_dot_config/exact_nvim/lua/exact_plugins/inc-rename.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/inc-rename.lua
@@ -1,11 +1,5 @@
 ---@type LazyPluginSpec
 return {
   "smjonas/inc-rename.nvim",
-  dependencies = {
-    "stevearc/dressing.nvim",
-  },
   cmd = "IncRename",
-  opts = {
-    input_buffer_type = "dressing",
-  },
 }
