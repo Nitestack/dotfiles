21c5b02f3c44149aed4737cf1017537dff95c063
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-02 15:58:57 +0100

    feat(cli): add install command

---
+++ scripts/src/update_command.sh
@@ -0,0 +1,19 @@
+set -- update
+
+if [[ -n "${args["--refresh-externals"]}" ]]; then
+	set -- "$@" --refresh-externals="${args["--refresh-externals"]}"
+fi
+
+command_exec chezmoi "$@"
+
+log_success "Updated dotfiles"
+
+if [[ -n "${args["--cli"]}" ]]; then
+	script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+	updated_script_dir=$(realpath "$(eval echo "$(chezmoi source-path)")/../scripts/dotfiles")
+
+	command_exec cp -f "${updated_script_dir}" "${script_dir}"
+
+	log_success "Updated the CLI to the latest version"
+fi
