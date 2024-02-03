e583e0ebd11cc98bfb37d7a6b0f003ee96dc7167
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-03 22:48:08 +0100

    feat(cli): added pwsh library

--- scripts/windows/dotfiles.ps1
+++ scripts/windows/dotfiles.ps1
@@ -30,6 +30,84 @@ param(
   [switch]$Help
 )
 
+# PowerShell Core library
+
+function Write-LogColor
+{
+  param([string]$colorCode, [string]$message)
+  Write-Host -Object "$message" -ForegroundColor $colorCode
+}
+function Write-LogRed
+{
+  param([string]$message)
+  Write-LogColor -colorCode "Red" -message $message
+}
+function Write-LogBlue
+{
+  param([string]$message)
+  Write-LogColor -colorCode "Blue" -message $message
+}
+function Write-LogGreen
+{
+  param([string]$message)
+  Write-LogColor -colorCode "Green" -message $message
+}
+function Write-LogYellow
+{
+  param([string]$message)
+  Write-LogColor -colorCode "Yellow" -message $message
+}
+
+function Write-LogTask
+{
+  param([string]$message)
+  Write-LogBlue -message " TASK: $message"
+}
+function Write-ManualAction
+{
+  param([string]$message)
+  Write-LogRed -message " MANUAL ACTION: $message"
+}
+function Write-LogError
+{
+  param([string]$message)
+  Write-LogRed -message " ERROR:$message"
+}
+function Write-LogInfo
+{
+  param([string]$message)
+  Write-LogBlue -message " INFO: $message"
+}
+function Write-LogSuccess
+{
+  param([string]$message)
+  Write-LogGreen -message " SUCCESS: $message"
+}
+
+function Write-LogCommand
+{
+  param([string]$message)
+  Write-LogYellow -message " COMMAND: $message"
+}
+function Invoke-Command
+{
+  param([string]$message)
+  Write-LogCommand -message $message
+  try
+  {
+    Invoke-Expression $message
+  } catch
+  {
+    Write-LogError -message "Command failed: $_"
+  }
+}
+function Invoke-Error
+{
+  param([string]$message)
+  Write-LogError -message $message
+  exit 1
+}
+
 function Invoke-Download
 {
   <#
