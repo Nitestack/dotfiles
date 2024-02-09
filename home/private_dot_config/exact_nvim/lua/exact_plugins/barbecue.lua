25b3dc1548aad494a1bd57d9b78265a540642ebf
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-09 20:11:52 +0100

    feat(nvim): configure barbecue

--- home/private_dot_config/exact_nvim/lua/exact_plugins/barbecue.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/barbecue.lua
@@ -5,13 +5,20 @@ return {
   version = "*",
   event = "LazyFile",
   dependencies = {
-    "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons",
   },
   ---@type barbecue.Config
   opts = {
     theme = core.config.ui.theme,
     create_autocmd = false, -- prevent barbecue from updating itself automatically
+    show_modified = true,
+    symbols = {
+      modified = core.icons.ui.Circle,
+      elipsis = core.icons.ui.Ellipsis,
+    },
+    -- Disable navic completely
+    attach_navic = false,
+    show_navic = false,
   },
   config = function(_, opts)
     require("barbecue").setup(opts)
