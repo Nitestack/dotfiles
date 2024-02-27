8cb9b5cea409ed809a5a355c4ed2bd0f7d0d0b2d
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-02-28 00:47:27 +0100

    feat(nvim): add lsp auto command to clear highlights

--- home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-lspconfig.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-lspconfig.lua
@@ -142,6 +142,22 @@ return {
               buffer = args.buf,
               silent = true,
             })
+
+            -- The following two autocommands are used to highlight references of the
+            -- word under your cursor when your cursor rests there for a little while.
+            -- When you move your cursor, the highlights will be cleared (the second autocommand).
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and client.server_capabilities.documentHighlightProvider then
+              vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                buffer = args.buf,
+                callback = vim.lsp.buf.document_highlight,
+              })
+
+              vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                buffer = args.buf,
+                callback = vim.lsp.buf.clear_references,
+              })
+            end
           end,
         },
       },
