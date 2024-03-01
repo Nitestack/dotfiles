bcebefdd81485ecba23f2d1a664ef507f7d3c8e9
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-01 19:47:57 +0100

    feat(nvim): change treesitter context highlights

--- home/private_dot_config/exact_nvim/lua/exact_plugins/catppuccin.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/catppuccin.lua
@@ -19,45 +19,24 @@ return {
       keywords = { "italic" },
     },
     custom_highlights = function(C)
-      local U = require("catppuccin.utils.colors")
+      local treesitter_context_bg = C.base
+
       ---@type { [string]: CtpHighlight}
-      local highlights = {
+      return {
         MiniIndentscopeSymbol = {
           fg = "#737aa2",
         },
+        TreesitterContext = {
+          bg = treesitter_context_bg,
+        },
+        TreesitterContextLineNumber = {
+          fg = C.surface1,
+          bg = treesitter_context_bg,
+        },
+        TreesitterContextBottom = {
+          style = {},
+        },
       }
-
-      if not core.config.ui.transparent.floats then
-        ---@type { [string]: CtpHighlight}
-        local float_highlights = {
-          NormalFloat = { bg = C.mantle },
-          Pmenu = { bg = U.darken(C.surface0, 0.8, C.crust) },
-
-          NeoTreeNormal = { bg = C.mantle },
-          NeoTreeNormalNC = { bg = C.mantle },
-          NeoTreeVertSplit = { bg = C.base },
-
-          DiagnosticVirtualTextError = { bg = U.darken(C.red, 0.095, C.base) },
-          DiagnosticVirtualTextWarn = { bg = U.darken(C.yellow, 0.095, C.base) },
-          DiagnosticVirtualTextInfo = { bg = U.darken(C.sky, 0.095, C.base) },
-          DiagnosticVirtualTextHint = { bg = U.darken(C.teal, 0.095, C.base) },
-
-          TelescopeBorder = { fg = C.mantle, bg = C.mantle },
-          TelescopeNormal = { bg = C.mantle },
-          TelescopePromptBorder = { fg = C.surface0, bg = C.surface0 },
-          TelescopePromptNormal = { bg = C.surface0 },
-          TelescopePromptPrefix = { bg = C.surface0 },
-          TelescopePreviewTitle = { fg = C.base, bg = C.green },
-          TelescopePromptTitle = { fg = C.base, bg = C.red },
-          TelescopeResultsTitle = { fg = C.mantle, bg = C.lavender },
-          TelescopeSelection = { fg = C.text, bg = C.surface0 },
-
-          TreesitterContext = { bg = C.none, fg = C.text, link = "" },
-        }
-        highlights = vim.tbl_deep_extend("force", highlights, float_highlights) --[[@as { [string]: CtpHighlight}]]
-      end
-
-      return highlights
     end,
     integrations = {
       barbecue = {
