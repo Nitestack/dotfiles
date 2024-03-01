2563ccbf3f6219541d82535a017e0f11102171ac
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-01 22:09:09 +0100

    feat(nvim): remove duplications, preset has same keymaps

--- home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-cmp.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-cmp.lua
@@ -38,12 +38,9 @@ return {
 
       -- Setup mappings
       local mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
           if luasnip.expand_or_locally_jumpable() then
