36865ef921d0a31e6ae865608d4037e9c9fbc9c8
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-03 01:17:35 +0100

    refactor(nvim): move lsp aspects to utils

--- home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-lspconfig.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/exact_lsp/nvim-lspconfig.lua
@@ -1,32 +1,4 @@
----@param opts? lsp.Client.filter
-local function get_clients(opts)
-  local ret = {} ---@type lsp.Client[]
-  if vim.lsp.get_clients then
-    ret = vim.lsp.get_clients(opts)
-  else
-    ---@diagnostic disable-next-line: deprecated
-    ret = vim.lsp.get_active_clients(opts)
-    if opts and opts.method then
-      ---@param client lsp.Client
-      ret = vim.tbl_filter(function(client)
-        return client.supports_method(opts.method, { bufnr = opts.bufnr })
-      end, ret)
-    end
-  end
-  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
-end
-
----@param method string
-local function client_supports_method(method, buffer)
-  method = method:find("/") and method or "textDocument/" .. method
-  local clients = get_clients({ bufnr = buffer })
-  for _, client in ipairs(clients) do
-    if client.supports_method(method) then
-      return true
-    end
-  end
-  return false
-end
+local lsp_utils = require("utils.lsp")
 
 ---@type LazyPluginSpec
 return {
@@ -92,28 +64,15 @@ return {
     vim.diagnostic.config(diagnostic_opts)
 
     -- Setup language servers
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities =
-      vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), cmp_nvim_lsp.default_capabilities(), {
-        workspace = {
-          -- PERF: didChangeWatchedFiles is too slow.
-          -- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
-          didChangeWatchedFiles = { dynamicRegistration = false },
-        },
-      })
+    local capabilities = lsp_utils.get_capabilities({
+      workspace = {
+        -- PERF: didChangeWatchedFiles is too slow.
+        -- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
+        didChangeWatchedFiles = { dynamicRegistration = false },
+      },
+    })
 
-    -- Jump directly to the first available definition every time.
-    vim.lsp.handlers["textDocument/definition"] = function(_, result)
-      if not result or vim.tbl_isempty(result) then
-        return
-      end
-
-      if vim.tbl_islist(result) then
-        vim.lsp.util.jump_to_location(result[1], "utf-8")
-      else
-        vim.lsp.util.jump_to_location(result, "utf-8")
-      end
-    end
+    lsp_utils.set_handlers()
 
     require("mason-lspconfig").setup({
       handlers = {
@@ -146,47 +105,14 @@ return {
             ---@type Mappings
             local mappings = require("config.mappings").lsp_mappings(args)
 
-            -- If client doesn't support method, remove it from mappings
-            for _, mode in pairs(mappings) do
-              for mapping, mapping_info in pairs(mode) do
-                if mapping_info.has and not client_supports_method(mapping_info.has, args.buf) then
-                  mode[mapping] = nil
-                else
-                  mapping_info.has = nil
-                  mapping_info[2] = "LSP: " .. mapping_info[2]
-                end
-              end
-            end
+            lsp_utils.remove_unsupported_methods(args.buf, mappings)
 
             core.map(mappings, {
               buffer = args.buf,
               silent = true,
             })
 
-            -- The following two autocommands are used to highlight references of the
-            -- word under your cursor when your cursor rests there for a little while.
-            -- When you move your cursor, the highlights will be cleared (the second autocommand).
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            if client and client.server_capabilities.documentHighlightProvider then
-              core.auto_cmds({
-                {
-                  { "CursorHold", "CursorHoldI" },
-                  {
-                    group = "lsp_document_highlight",
-                    buffer = args.buf,
-                    callback = vim.lsp.buf.document_highlight,
-                  },
-                },
-                {
-                  { "CursorMoved", "CursorMovedI" },
-                  {
-                    group = "lsp_document_highlight",
-                    buffer = args.buf,
-                    callback = vim.lsp.buf.clear_references,
-                  },
-                },
-              })
-            end
+            lsp_utils.set_document_highlight(args)
           end,
         },
       },
