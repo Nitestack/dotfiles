c4ceebffa843cc4b944cd44c65789ac35f5433ea
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-26 23:02:38 +0100

    fix(nvim): remove duplicate field in telescope

--- home/private_dot_config/exact_nvim/lua/exact_plugins/telescope.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/telescope.lua
@@ -10,8 +10,9 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = vim.fn.executable("make") == 1 and "make"
         or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-      enabled = vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1,
-      enabled = function() return vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1 end,
+      enabled = function()
+        return vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1
+      end,
     },
   },
   keys = core.lazy_map({
