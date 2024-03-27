e78b242a0904e36cff7bd57049c53747c11b0d2f
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-27 01:24:27 +0100

    feat(nvim): auto-create for Windows too

--- home/private_dot_config/exact_nvim/lua/config/commands.lua
+++ home/private_dot_config/exact_nvim/lua/config/commands.lua
@@ -157,13 +157,13 @@ M.auto_cmds = {
     {
       group = "auto_create_dir",
       callback = function(event)
-        if event.match:match("^%w%w+://") then
+        if event.match:match("^%w%w+:[\\/][\\/]") then
           return
         end
-        local file = vim.loop.fs_realpath(event.match) or event.match
+        local file = vim.uv.fs_realpath(event.match) or event.match
         vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
       end,
-    },
+    }
   },
   -- Remove any trailing whitespace on save
   {
