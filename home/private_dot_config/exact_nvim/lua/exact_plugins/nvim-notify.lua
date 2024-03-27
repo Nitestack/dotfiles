619c4fb913fcc05d6f06916e8ec3659b09728d22
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-03-27 01:35:57 +0100

    feat(nvim): add performance mode to nvim-notify

--- home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-notify.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-notify.lua
@@ -11,7 +11,9 @@ return {
       },
     },
   }),
+  ---@type notify.Config
   opts = {
+    stages = core.config.performance_mode and "static" or "fade_in_slide_out",
     timeout = 3000,
     max_height = function()
       return math.floor(vim.o.lines * 0.75)
@@ -22,7 +24,7 @@ return {
     on_open = function(win)
       vim.api.nvim_win_set_config(win, { zindex = 100 })
     end,
-    fps = 144,
+    fps = not core.config.performance_mode and 144 or 30,
     icons = core.icons.diagnostics,
   },
 }
