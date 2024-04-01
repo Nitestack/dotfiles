90299a185be4275c93676524c1489a9da79d4fd8
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-01 02:28:35 +0200

    refactor(nvim): document settings

--- home/private_dot_config/exact_nvim/lua/config/settings.lua
+++ home/private_dot_config/exact_nvim/lua/config/settings.lua
@@ -15,50 +15,88 @@ local M = {}
 --  Options
 --------------------------------------------------------------------------------
 M.options = {
-  -- Clipboard
-  autowrite = true,
-  clipboard = "unnamedplus",
-  completeopt = "menu,menuone,noselect",
-  conceallevel = 2,
-  confirm = true,
-  cursorline = true,
-  expandtab = true,
-  formatoptions = "jqlnt",
-  grepformat = "%f:%l:%c:%m",
-  grepprg = "rg --vimgrep",
-  helplang = "de",
-  ignorecase = true,
-  inccommand = "nosplit",
-  laststatus = 3,
-  mouse = "a",
-  pumheight = 10,
-  scrolloff = 4,
-  shiftround = true,
-  shiftwidth = 2,
-  showmode = false,
-  sidescrolloff = 8,
-  signcolumn = "yes",
-  smartcase = true,
-  smartindent = true,
-  smoothscroll = true,
-  spelllang = { "en" },
-  splitbelow = true,
-  splitkeep = "screen",
-  splitright = true,
-  tabstop = 2,
-  termguicolors = true,
-  timeoutlen = 300,
-  updatetime = 200,
+  autowrite = true,                      -- Writes modified files on certain commands and navigation to other files
+  completeopt = "menu,menuone,noselect", -- Set completion options for popup menu
+  conceallevel = 2,                      -- Hide * markup for bold and italic, but not markers with substitutions
+  confirm = true,                        -- Confirm to save changes before exiting modified buffer
+  cursorline = true,                     -- Enable highlighting of the current line
+  formatoptions = "jqlnt",               -- Formatting options
+  helplang = "de",                       -- Set the language for the help messages
+  inccommand = "nosplit",                -- Preview incremental substitute
+  laststatus = 3,                        -- Always show statusline
+  mouse = "a",                           -- Enable mouse usage
+  pumheight = 10,                        -- Maximum number of items in the popup menu
+  sessionoptions = {                     -- Save options for sessions
+    "buffers",                           --
+    "curdir",                            --
+    "tabpages",                          --
+    "winsize",                           --
+    "help",                              --
+    "globals",                           --
+    "skiprtp",                           --
+    "folds"                              --
+  },                                     --
+  showmode = false,                      -- Dont show mode since we have a statusline
+  signcolumn = "yes",                    -- Always draw the sign column, avoiding layout shifts
+  smoothscroll = true,                   -- Enables smooth scrolling with screen lines.
+  termguicolors = true,                  -- Enable 24-bit RGB colors in the TUI
+  winminwidth = 5,                       -- Minimum window width
+
+  -- Backup
+  backup = false,                            -- Disable backup files
+  swapfile = false,                          -- Disable swap file
+  undodir = vim.fn.expand("~/.vim/undodir"), -- Directory for undo files
+  undofile = true,                           -- Enable persistent undo
+  undolevels = 10000,                        -- Maximum number of changes that can be undone
+
+  -- GUI settings
+  guifont = "SF Mono,Symbols Nerd Font:h18", -- Set font for graphical Neovim apps
+  linespace = 6,                             -- Set a line height of `1.2`
+
+  -- Indentation
+  expandtab = true,   -- Use spaces instead of tabs
+  shiftround = true,  -- Round indent to multiple of 'shiftwidth'
+  shiftwidth = 2,     -- Number of spaces for each step of (auto)indent
+  smartindent = true, -- Do smart autoindenting when starting a new line
+  tabstop = 2,        -- Number of spaces that a <Tab> in the file counts for
+
+  -- Fill Characters
   fillchars = {
     foldopen = "",
     foldclose = "",
-    -- fold = "⸱",
     fold = " ",
     foldsep = " ",
     diff = "╱",
     eob = " ",
   },
-  wildignore = {
+
+  -- Format for grep
+  grepformat = "%f:%l:%c:%m", -- Format for grep
+  grepprg = "rg --vimgrep",   -- Set the grep program for :grep
+
+  -- Search
+  ignorecase = true, -- Ignore case in search patterns
+  smartcase = true,  -- Override 'ignorecase' if the search pattern contains uppercase characters
+
+  -- Line Numbers
+  number = true,         -- Show line numbers
+  relativenumber = true, -- Show line numbers relative to the current line
+
+  -- Scroll off
+  scrolloff = 4,     -- Number of screen lines to keep above and below the cursor
+  sidescrolloff = 8, -- Number of screen columns to keep to the left and right of the cursor
+
+  -- Split Windows
+  splitbelow = true,    -- Put new windows below current
+  splitright = true,    -- Put new windows right of the current one
+  splitkeep = "screen", -- Keep splits open even if they are empty
+
+  -- Timing
+  timeoutlen = 300, -- Time to wait for a mapped sequence to complete (in milliseconds)
+  updatetime = 200, -- Number of milliseconds to wait for the next typed character in insert mode
+
+  -- File Management
+  wildignore = { -- Patterns to ignore during file navigation
     "**/node_modules/**",
     "**/.git/**",
     "**/dist/**",
@@ -70,29 +108,11 @@ M.options = {
     "**/dist/**",
   },
 
-  -- Number
-  number = true,
-  relativenumber = true,
-
-  -- Session
-  sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" },
-
-  -- Backup
-  swapfile = false,
-  backup = false,
-  undodir = vim.fn.expand("~/.vim/undodir"),
-  undofile = true,
-  undolevels = 10000,
-
   -- Wrap
-  wrap = true,
-  linebreak = true,
-  breakindent = true,
-  showbreak = core.icons.ui.Tab .. " ",
-
-  -- GUI settings
-  guifont = "SF Mono,Symbols Nerd Font:h18", -- set font for graphical Neovim apps
-  linespace = 6,
+  breakindent = true,                   -- Indent wrapped lines visually
+  linebreak = true,                     -- Break line at convenient point
+  showbreak = core.icons.ui.Tab .. " ", -- Character displayed for the break
+  wrap = true,                          -- Wrap lines
 }
 
 --------------------------------------------------------------------------------
@@ -100,7 +120,7 @@ M.options = {
 --------------------------------------------------------------------------------
 M.globals = {
   mapleader = " ",
-  maplocalleader = "\\",
+  maplocalleader = "\\", -- <LocalLeader> is to be used for mappings which are local to a buffer.
   markdown_recommended_style = 0,
   skip_ts_context_commentstring_module = true,
 }
@@ -114,25 +134,37 @@ M.disabled_providers = { "perl", "ruby", "node", "python3" }
 --  Additional settings
 --------------------------------------------------------------------------------
 function M.run()
-  -- Neovide
+  -- Neovide settings
   if core.is_neovide() then
     vim.g.neovide_hide_mouse_when_typing = true
   end
 
-  -- undercurl
+  -- Only set clipboard if not in ssh, to make sure the OSC 52 integration works automatically
+  if not vim.env.SSH_TTY then
+    vim.opt.clipboard = "unnamedplus"
+  end
+
+  -- Enable undercurl
   vim.cmd([[let &t_Cs = "\e[4:3m"]])
   vim.cmd([[let &t_Ce = "\e[4:0m"]])
-  -- go to previous/next line with h,l,left arrow and right arrow when cursor reaches end/beginning of line
+  -- Go to previous/next line with h/l/left arrow/right arrow when cursor reaches end/beginning of line
   vim.opt.whichwrap:append("<>[]hl")
-  -- add binaries installed by mason.nvim to path
+  -- Add binaries installed by `mason.nvim` to path
   vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin" .. (core.is_win() and ";" or ":") .. vim.env.PATH
 
-  -- TailwindCSS wraps
+  -- For Tailwind CSS wraps
   vim.opt.breakat:remove({ ":", "/", "-" })
 
-  vim.opt.shortmess:append({ a = true, W = true, I = true, c = true, C = true })
+  -- Modify shortmess
+  vim.opt.shortmess:append({
+    a = true, -- append `l`, `m`, `r`, `w` abbreviations
+    W = true, -- don't give "written" or "[w]" when writing a file
+    A = true, -- don't give the "ATTENTION" message when an existing swap file is found
+    I = true, -- don't give the intro message when starting Vim, see |:intro|
+    c = true, -- don't give |ins-completion-menu| messages; for example, "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found", "Back at original", etc.
+  })
 
-  -- Windows
+  -- Setup Windows shell
   if core.is_win() then
     -- Use PowerShell Core instead of Windows CMD
     vim.o.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
@@ -144,9 +176,9 @@ function M.run()
     vim.o.shellxquote = ""
   end
 
-  -- WSL
+  -- Setup WSL clipboard
   if core.is_wsl() then
-    -- Sync clipboard with Windows clipboard
+    -- Sync WSL clipboard with Windows clipboard
     vim.g.clipboard = {
       name = "WslClipboard",
       copy = {
