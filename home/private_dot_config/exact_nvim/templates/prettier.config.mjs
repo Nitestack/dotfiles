805ab1d773c6d54f11b7b6670ac845cb930288ca
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-03 19:56:06 +0200

    feat(prettier): new prettier template

--- home/private_dot_config/exact_nvim/templates/prettier.config.mjs
+++ home/private_dot_config/exact_nvim/templates/prettier.config.mjs
@@ -1,14 +1,36 @@
+import tsConfig from "./tsconfig.json" with { type: "json" };
+
+const THIRD_PARTY_MODULES = "<THIRD_PARTY_MODULES>"; // Imports not matched by other special words or groups
+const BUILTIN_MODULES = "<BUILTIN_MODULES>"; // Node.js built-in modules
+const RELATIVE_IMPORTS = "^[.]"; // Relative imports
+const TYPES = {
+  NODE: "<TYPES>^(node:)", // Types from Node.js built-in modules
+  THIRD_PARTY: "<TYPES>", // Types from third party modules
+  RELATIVE: "<TYPES>^[.]", // Types from relative imports
+};
+
+/** @type {string[]} */
+const ALIASES = Object.keys(tsConfig.compilerOptions?.paths ?? {}).map((path) =>
+  path.replace(/\*\//g, "")
+); // Specify import aliases
+
 /** @type {import('prettier').Config} */
 export default {
-  useTabs: false,
-  tabWidth: 2,
-  singleQuote: false,
-  semi: true,
   trailingComma: "es5",
-  bracketSpacing: true,
-  bracketSameLine: false,
-  singleAttributePerLine: false,
-  jsxSingleQuote: false,
-  quoteProps: "as-needed",
-  arrowParens: "always",
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  // INFO: To group imports into "chunks" with blank lines between, add empty strings
+  importOrder: [
+    "",
+    BUILTIN_MODULES,
+    "",
+    THIRD_PARTY_MODULES,
+    "",
+    `^(${ALIASES.join("|")})(/.*)$`,
+    RELATIVE_IMPORTS,
+    "",
+    TYPES.NODE,
+    TYPES.THIRD_PARTY,
+    TYPES.RELATIVE,
+  ],
+  importOrderTypeScriptVersion: "5.4.3",
 };
