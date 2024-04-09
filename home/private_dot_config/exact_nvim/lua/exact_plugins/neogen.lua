67727f8e642240f9af5cd40b809b059fa0cd4fd0
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-09 23:12:47 +0200

    feat(nvim): add additional mappings for doc gen

--- home/private_dot_config/exact_nvim/lua/exact_plugins/neogen.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/neogen.lua
@@ -1,13 +1,26 @@
 ---@type LazyPluginSpec
 return {
   "danymat/neogen",
+  dependencies = { "L3MON4D3/LuaSnip" },
   keys = core.lazy_map({
     n = {
-      ["<leader>cd"] = {
+      ["<leader>cdf"] = {
         function()
-          require("neogen").generate({})
+          require("neogen").generate({ type = "func" })
         end,
-        "Docs: Generate annotations",
+        "Docs: Function annotation",
+      },
+      ["<leader>cdc"] = {
+        function()
+          require("neogen").generate({ type = "class" })
+        end,
+        "Docs: Class annotations",
+      },
+      ["<leader>cdt"] = {
+        function()
+          require("neogen").generate({ type = "type" })
+        end,
+        "Docs: Type annotations",
       },
     },
   }),
