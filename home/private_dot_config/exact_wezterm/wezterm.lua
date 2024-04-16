36f3ea4e5b59ad38a528a574464d2b54511bd892
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-16 23:07:39 +0200

    refactor(wezterm): include type definitions

--- home/private_dot_config/exact_wezterm/wezterm.lua
+++ home/private_dot_config/exact_wezterm/wezterm.lua
@@ -8,11 +8,13 @@ require("config.general").setup(config)
 require("config.keys").setup(config)
 require("config.platforms").setup(config)
 
-return smart_splits.apply_to_config(config, {
-  -- directional keys to use in order of: left, down, up, right
+---@type SmartSplitsWeztermConfig
+local smart_splits_config = {
   direction_keys = { "h", "j", "k", "l" },
   modifiers = {
-    move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
-    resize = "LEADER", -- modifier to use for pane resize, e.g. META+h to resize to the left
+    move = "CTRL",
+    resize = "LEADER",
   },
-})
+}
+
+return smart_splits.apply_to_config(config, smart_splits_config)
