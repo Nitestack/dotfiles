093657217514bfe939b7b3fa96358d7f10650036
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-20 13:58:53 +0200

    feat(nvim): highlight virtual text diagnostics

--- home/private_dot_config/exact_nvim/lua/exact_plugins/catppuccin.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/catppuccin.lua
@@ -16,10 +16,12 @@ return {
       keywords = { "italic" },
     },
     custom_highlights = function(C)
-      local treesitter_context_bg = C.base
+      local treesitter_context_bg = C.crust
+      local U = require("catppuccin.utils.colors")
 
       ---@type { [string]: CtpHighlight}
       return {
+        -- Treesitter context
         TreesitterContext = {
           bg = treesitter_context_bg,
         },
@@ -30,6 +32,11 @@ return {
         TreesitterContextBottom = {
           style = {},
         },
+        -- Diagnostics
+        DiagnosticVirtualTextError = { bg = U.darken(C.red, 0.095, C.base) },
+        DiagnosticVirtualTextWarn = { bg = U.darken(C.yellow, 0.095, C.base) },
+        DiagnosticVirtualTextInfo = { bg = U.darken(C.sky, 0.095, C.base) },
+        DiagnosticVirtualTextHint = { bg = U.darken(C.teal, 0.095, C.base) },
       }
     end,
     integrations = {
