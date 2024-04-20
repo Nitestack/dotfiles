c48c1d2179659fc71a391a63b571bd19302a53cd
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-20 13:58:43 +0200

    feat(nvim): change `prefix` to `<leader>a` for annotations

--- home/private_dot_config/exact_nvim/lua/exact_plugins/neogen.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/neogen.lua
@@ -1,32 +1,49 @@
----@type LazyPluginSpec
+---@type LazySpec
 return {
-  "danymat/neogen",
-  dependencies = { "L3MON4D3/LuaSnip" },
-  keys = core.lazy_map({
-    n = {
-      ["f"] = {
-        function()
-          require("neogen").generate({ type = "func" })
-        end,
-        "Docs: Function annotation",
+  {
+    "danymat/neogen",
+    dependencies = "L3MON4D3/LuaSnip",
+    cmd = "Neogen",
+    keys = core.lazy_map({
+      n = {
+        ["f"] = {
+          function()
+            require("neogen").generate({ type = "func" })
+          end,
+          "Annotation: Function",
+        },
+        ["F"] = {
+          function()
+            require("neogen").generate({ type = "file" })
+          end,
+          "Annotation: File",
+        },
+        ["c"] = {
+          function()
+            require("neogen").generate({ type = "class" })
+          end,
+          "Annotation: Class",
+        },
+        ["t"] = {
+          function()
+            require("neogen").generate({ type = "type" })
+          end,
+          "Annotation: Type",
+        },
       },
-      ["c"] = {
-        function()
-          require("neogen").generate({ type = "class" })
-        end,
-        "Docs: Class annotations",
-      },
-      ["t"] = {
-        function()
-          require("neogen").generate({ type = "type" })
-        end,
-        "Docs: Type annotations",
+    }, {
+      prefix = "<leader>a",
+    }),
+    opts = {
+      snippet_engine = "luasnip",
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>a"] = { name = "+annotations" },
       },
     },
-  }, {
-    prefix = "<leader>cd",
-  }),
-  opts = {
-    snippet_engine = "luasnip",
   },
 }
