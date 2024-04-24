e7accc5f8271af7ddcbc770d12b661d95e1cf748
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-04-24 18:58:54 +0200

    feat(nvim): remove deprecated and unnecessary `vim.lsp.get_active_clients`

--- home/private_dot_config/exact_nvim/lua/exact_utils/lsp.lua
+++ home/private_dot_config/exact_nvim/lua/exact_utils/lsp.lua
@@ -30,28 +30,10 @@ function M.set_handlers()
   end
 end
 
----@param opts? lsp.Client.filter
-local function get_clients(opts)
-  local ret = {} ---@type lsp.Client[]
-  if vim.lsp.get_clients then
-    ret = vim.lsp.get_clients(opts)
-  else
-    ---@diagnostic disable-next-line: deprecated
-    ret = vim.lsp.get_active_clients(opts)
-    if opts and opts.method then
-      ---@param client lsp.Client
-      ret = vim.tbl_filter(function(client)
-        return client.supports_method(opts.method, { bufnr = opts.bufnr })
-      end, ret)
-    end
-  end
-  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
-end
-
 ---@param method string
 local function client_supports_method(method, buffer)
   method = method:find("/") and method or "textDocument/" .. method
-  local clients = get_clients({ bufnr = buffer })
+  local clients = vim.lsp.get_clients({ bufnr = buffer })
   for _, client in ipairs(clients) do
     if client.supports_method(method) then
       return true
@@ -111,7 +93,7 @@ end
 ---@param from string
 ---@param to string
 function M.on_rename(from, to)
-  local clients = get_clients()
+  local clients = vim.lsp.get_clients()
   for _, client in ipairs(clients) do
     if client.supports_method("workspace/willRenameFiles") then
       ---@diagnostic disable-next-line: invisible
