b1e5830f26bf6cff8132eab8c6af7bb9a8158c89
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-13 22:30:46 +0200

    feat(win): add new packages ui

--- home/.chezmoitemplates/pwsh-library.ps1
+++ home/.chezmoitemplates/pwsh-library.ps1
@@ -1,5 +1,38 @@
 # PowerShell Core library
 
+function Start-Loading
+{
+  param([string]$message, [string]$colorCode)
+  if ([string]::IsNullOrEmpty($colorCode))
+  {
+    $colorCode = "Yellow"
+  }
+
+  Write-Host -NoNewline -ForegroundColor $colorCode "󰇘$message"
+}
+
+function Stop-Loading
+{
+  param([string]$message, [string]$colorCode, [switch]$isSuccess)
+  if ([string]::IsNullOrEmpty($colorCode))
+  {
+    $colorCode = "Green"
+  }
+
+  if ($isSuccess)
+  {
+    $emoji = ""
+  }
+  else
+  {
+    $emoji = ""
+  }
+
+  $padding = " " * [Console]::WindowWidth
+  Write-Host -NoNewline "`r$padding"
+  Write-Host -ForegroundColor $colorCode "`r$emoji $message"
+}
+
 function Write-LogColor
 {
   param([string]$colorCode, [string]$message)
@@ -29,46 +62,45 @@ function Write-LogYellow
 function Write-LogTask
 {
   param([string]$message)
-  Write-LogBlue -message " TASK: $message"
+  Write-LogBlue -message " $message"
 }
 function Write-ManualAction
 {
   param([string]$message)
-  Write-LogRed -message " MANUAL ACTION: $message"
+  Write-LogRed -message " $message"
 }
 function Write-LogError
 {
   param([string]$message)
-  Write-LogRed -message " ERROR: $message"
+  Write-LogRed -message " $message"
 }
 function Write-LogInfo
 {
   param([string]$message)
-  Write-LogBlue -message " INFO: $message"
+  Write-LogBlue -message " $message"
 }
 function Write-LogSuccess
 {
   param([string]$message)
-  Write-LogGreen -message " SUCCESS: $message"
+  Write-LogGreen -message " $message"
 }
 function Write-LogWarning
 {
   param([string]$message)
-  Write-LogYellow -message " WARNING: $message"
+  Write-LogYellow -message " $message"
 }
 
 function Write-LogCommand
 {
   param([string]$message)
-  Write-LogYellow -message " COMMAND: $message"
+  Write-LogYellow -message " $message"
 }
 function Invoke-CommandExpression
 {
-  param([string]$message)
-  Write-LogCommand -message $message
+  param([string]$cmd)
   try
   {
-    Invoke-Expression $message
+    Invoke-Expression $cmd
   } catch
   {
     Write-LogError -message "Command failed: $_"
@@ -85,38 +117,46 @@ function Invoke-Error
 function Invoke-EnsureInstalled
 {
   param([string]$cmd_name, [string]$command)
+  Start-Loading $cmd_name
+
   if (Get-Command $cmd_name -ErrorAction SilentlyContinue)
   {
-    Write-LogInfo "$cmd_name is already installed. Skipping."
+    Stop-Loading $cmd_name
     return
   }
 
-  Write-LogTask "Installing $cmd_name"
   Invoke-CommandExpression $command
+  Stop-Loading $cmd_name
 }
 
 function Invoke-ChocoEnsureInstalled
 {
   param([string]$package_name)
+  Start-Loading $package_name
+
   if (choco list --lo -r -e $package_name)
   {
-    Write-LogInfo "choco: Package '$package_name' is already installed. Skipping."
+    Stop-Loading $package_name
     return
   }
 
-  Write-LogTask "choco: Installing package '$package_name'"
   Invoke-CommandExpression "choco install -y $package_name"
+  Stop-Loading $package_name -isSuccess
 }
 
 function Invoke-WingetEnsureInstalled
 {
-  param([string]$package_name)
-  if (winget list --id $package_name)
+  param([string]$package_id)
+  $package_name = ($package_id -split "\.")[-1]
+
+  Start-Loading $package_name
+
+  if (winget list --id $package_id)
   {
-    Write-LogInfo "winget: Package '$package_name' is already installed. Skipping."
+    Stop-Loading $package_name
     return
   }
 
-  Write-LogTask "winget: Installing package '$package_name'"
-  Invoke-CommandExpression "winget install -e --accept-package-agreements --accept-source-agreements --id $package_name"
+  Invoke-CommandExpression "winget install -e --accept-package-agreements --accept-source-agreements --id $package_id"
+  Stop-Loading $package_name -isSuccess
 }
