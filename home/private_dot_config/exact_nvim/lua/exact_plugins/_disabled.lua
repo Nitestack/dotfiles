f3e6bd0deb34f9212434edc21e4d93b471969c4b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-23 01:36:42 +0200

    feat(nvim): disable more unused plugins

--- home/private_dot_config/exact_nvim/lua/exact_plugins/_disabled.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/_disabled.lua
@@ -8,4 +8,7 @@ end, {
   "akinsho/bufferline.nvim",
   "folke/flash.nvim",
   "nvim-neo-tree/neo-tree.nvim",
+  "pmizio/typescript-tools.nvim",
+  "folke/tokyonight.nvim",
+  "roobert/tailwindcss-colorizer-cmp.nvim",
 })
