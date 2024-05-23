dd8e6f59950bbdf72fb219e3a4ec2eec35d46da0
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-23 02:25:23 +0200

    feat(nvim): add `vim-illuminate` back

--- home/private_dot_config/exact_nvim/lua/exact_plugins/_extras.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/_extras.lua
@@ -2,5 +2,6 @@
 return {
   { import = "lazyvim.plugins.extras.util.dot" },
   { import = "lazyvim.plugins.extras.util.startuptime" },
+  { import = "lazyvim.plugins.extras.editor.illuminate" },
   { import = "lazyvim.plugins.extras.vscode" },
 }
