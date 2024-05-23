d2a915669a590ed49b782822fc47c219283649be
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-23 02:03:55 +0200

    fix(nvim): ensure treesitter parsers get installed

--- home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-treesitter.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/nvim-treesitter.lua
@@ -13,40 +13,39 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, core.config.plugins.treesitter)
 
-      ---@type TSConfig
-      return {
-        highlight = {
-          -- disable highlighting in chezmoi templates
-          disable = function(_lang, buf)
-            -- check if 'filetype' option includes 'chezmoitmpl'
-            if string.find(vim.bo.filetype, "chezmoitmpl") then
-              return true
-            end
+      opts.highlight = opts.highlight or {}
+      -- disable highlighting in chezmoi templates
+      opts.highlight.disable = function(_lang, buf)
+        -- check if 'filetype' option includes 'chezmoitmpl'
+        if string.find(vim.bo.filetype, "chezmoitmpl") then
+          return true
+        end
 
-            -- check for file size
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-          additional_vim_regex_highlighting = false,
-        },
-        -- Auto-install missing parsers on startup
-        auto_install = true,
-        indent = {
-          -- Disable indent on certain file types
-          disable = { "yaml" },
-        },
-        -- configure vim-matchup
-        matchup = {
-          enable = true,
-          disable_virtual_text = true,
-        },
-        -- configure nvim-treesitter-endwise
-        endwise = {
-          enable = true,
-        },
+        -- check for file size
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end
+      opts.highlight.additional_vim_regex_highlighting = false
+
+      -- Auto-install missing parsers on startup
+      opts.auto_install = true
+
+      opts.indent = opts.indent or {}
+      -- Disable indent on certain file types
+      opts.indent.disable = vim.list_extend(opts.indent.disable or {}, { "yaml" })
+
+      -- configure vim-matchup
+      opts.matchup = {
+        enable = true,
+        disable_virtual_text = true,
+      }
+
+      -- configure nvim-treesitter-endwise
+      opts.endwise = {
+        enable = true,
       }
     end,
   },
