1d0f854ae8cf795833eed842cf8110f8b798b6ce
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-23 02:40:47 +0200

    feat(nvim): use kind icons for `trouble.nvim`

--- home/private_dot_config/exact_nvim/lua/exact_plugins/trouble.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/trouble.lua
@@ -8,6 +8,11 @@ return {
     opts = {
       auto_close = true,
       focus = true,
+      icons = {
+        kinds = vim.tbl_map(function(icon)
+          return icon .. " "
+        end, core.icons.kind),
+      },
     },
   },
 }
