ac8b3fa60b154fb526abaa85351d41207dd32351
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-24 01:46:04 +0200

    feat(nvim): remove lazygit from toggleterm as LazyVim natively supports lazygit

--- home/private_dot_config/exact_nvim/lua/exact_plugins/toggleterm.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/toggleterm.lua
@@ -20,11 +20,6 @@ return {
         desc = "Terminal: Toggle",
       },
     },
-    n = {
-      ["<leader>gg"] = {
-        desc = "Git: LazyGit",
-      },
-    },
   }),
   ---@param opts ToggleTermConfig
   opts = function(_, opts)
@@ -49,26 +44,5 @@ return {
     opts.float_opts.border = core.config.ui.transparent.floats and "curved" or "none"
     opts.width = math.floor(core.config.ui.width * vim.fn.winwidth(0))
     opts.height = math.floor(core.config.ui.height * vim.fn.winheight(0))
-
-    local lazygit = require("toggleterm.terminal").Terminal:new({
-      cmd = "lazygit",
-      dir = vim.uv.cwd(), -- `git_dir` is really slow
-      direction = "float",
-      hidden = true,
-      float_opts = {
-        border = core.config.ui.transparent.floats and "curved" or "none",
-        width = math.floor(0.9 * vim.fn.winwidth(0)),
-        height = math.floor(0.9 * vim.fn.winheight(0)),
-      },
-      on_open = function(term)
-        if vim.fn.mapcheck("<Esc>", "t") ~= "" then
-          vim.keymap.del("t", "<Esc>", { buffer = term.bufnr })
-        end
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>gg", function()
-      lazygit:toggle()
-    end, { desc = "Git: Lazygit", noremap = true, silent = true })
   end,
 }
