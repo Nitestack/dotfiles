72999b8beb7806001bf969e6ec5ee19a847fd1b1
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-05-31 01:36:02 +0200

    feat(nvim): `dev` branch was merged with `main`

--- home/private_dot_config/exact_nvim/lua/exact_plugins/trouble.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/trouble.lua
@@ -1,22 +1,18 @@
----@type LazySpec
+---@type LazyPluginSpec
 return {
-  { import = "lazyvim.plugins.extras.editor.trouble-v3" },
-  {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    keys = core.lazy_map({
-      n = {
-        [{ "<leader>cs", "<leader>cS" }] = { false },
-      },
-    }),
-    ---@type trouble.Config
-    opts = {
-      focus = true,
-      icons = {
-        kinds = vim.tbl_map(function(icon)
-          return icon .. " "
-        end, core.icons.kind),
-      },
+  "folke/trouble.nvim",
+  keys = core.lazy_map({
+    n = {
+      [{ "<leader>cs", "<leader>cS" }] = { false },
+    },
+  }),
+  ---@type trouble.Config
+  opts = {
+    focus = true,
+    icons = {
+      kinds = vim.tbl_map(function(icon)
+        return icon .. " "
+      end, core.icons.kind),
     },
   },
 }
