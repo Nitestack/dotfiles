6e04b1b8cd8d031b9d36ad3ac01e4ca67bf78584
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-07 23:06:52 +0200

    fix(nvim): remove better backup because it doesn't work well with special character entries

--- home/private_dot_config/exact_nvim/lua/exact_core/cmds.lua
+++ home/private_dot_config/exact_nvim/lua/exact_core/cmds.lua
@@ -72,19 +72,6 @@ M.auto_cmds = {
       end,
     },
   },
-  -- Better backup
-  {
-    "BufWritePre",
-    {
-      group = vim.api.nvim_create_augroup("better_backup", { clear = true }),
-      callback = function(event)
-        local file = vim.uv.fs_realpath(event.match) or event.match
-        local backup = vim.fn.fnamemodify(file, ":p:~:h")
-        backup = backup:gsub("[/\\]", "%%")
-        vim.go.backupext = backup
-      end,
-    },
-  },
   -- Enable breadcrumbs
   {
     {
