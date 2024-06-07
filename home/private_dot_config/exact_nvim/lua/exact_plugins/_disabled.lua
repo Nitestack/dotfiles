20a54adeb94e0d1792f15596607dda23b61a2d8b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-07 14:31:05 +0200

    feat(nvim): add `neo-tree` back, but as a secondary file explorer

--- home/private_dot_config/exact_nvim/lua/exact_plugins/_disabled.lua
+++ home/private_dot_config/exact_nvim/lua/exact_plugins/_disabled.lua
@@ -7,7 +7,6 @@ return vim.tbl_map(function(plugin_name)
 end, {
   "akinsho/bufferline.nvim",
   "folke/flash.nvim",
-  "nvim-neo-tree/neo-tree.nvim",
   "folke/tokyonight.nvim",
   "roobert/tailwindcss-colorizer-cmp.nvim",
 })
