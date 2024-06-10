01f2bde074efbe1e76e82a534bb73f21b9d32f45
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-10 19:08:22 +0200

    fix(nvim): use vim.filetype.get_option (`ts-comments.nvim` modification)

--- home/private_dot_config/exact_nvim/lua/exact_utils/snippet.lua
+++ home/private_dot_config/exact_nvim/lua/exact_utils/snippet.lua
@@ -5,13 +5,14 @@
 local M = {}
 
 function M.header_lines()
-  local comment = string.format(vim.bo.commentstring:gsub(" ", "") or "#%s", "-")
+  local comment = string.format(vim.filetype.get_option(vim.bo.filetype, "commentstring"):gsub(" ", "") or "#%s", "-")
   local col = vim.bo.textwidth ~= 0 and vim.bo.textwidth or 80
   return comment .. string.rep("-", col - #comment)
 end
 
 function M.header_title()
-  return vim.bo.commentstring:gsub("%%s", "")
+  local header_comment, _ = vim.filetype.get_option(vim.bo.filetype, "commentstring"):gsub("%%s", "")
+  return header_comment
 end
 
 return M
