9ec0c34fc0316677eb0f33986f2a0fa2fc4bb94b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-11 15:07:12 +0200

    feat(ags): add a TypeScript setup

--- home/private_dot_config/ags/config.js
+++ home/private_dot_config/ags/config.js
@@ -1,3 +1,22 @@
-App.config({
-  windows: [],
-});
+const main = "/tmp/asztal/main.js";
+const entry = `${App.configDir}/main.ts`;
+
+try {
+  await Utils.execAsync([
+    "esbuild",
+    "--bundle",
+    entry,
+    "--format=esm",
+    `--outfile=${main}`,
+    "--external:resource://*",
+    "--external:gi://*",
+    "--external:file://*",
+  ]);
+
+  await import(`file://${main}`);
+} catch (error) {
+  console.error(error);
+  App.quit();
+}
+
+export {};
