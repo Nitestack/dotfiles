0fe60fb5bfaac5d61bda2f55b4a59a2571a317d6
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-17 02:55:35 +0200

    feat(scripts): use `gum` and remove legacy logs and spinners

--- home/.chezmoitemplates/bash-library.sh
+++ home/.chezmoitemplates/bash-library.sh
@@ -5,132 +5,53 @@
 
 set -uo pipefail
 
-log_color() {
-	local color_code="$1"
+echo
+
+# Log functions
+_log() {
+	gum log -s "$@"
+}
+
+_spin() {
+	title=$1
 	shift
 
-	printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
-}
-log_red() {
-	log_color "0;31" "$@"
-}
-log_blue() {
-	log_color "0;34" "$@"
-}
-log_green() {
-	log_color "0;32" "$@"
-}
-log_info() {
-	echo "" "$@"
-}
-start_task() {
-	echo "󰪥" "$@"
-}
-complete_task() {
-	log_green "" "$@"
+	gum spin --show-error --spinner points --title "${title}" "$@"
 }
 
-show_spinner() {
-	local task="$1"
-	local start_message="$2"
-	local completion_message="$3"
-	local error_message="${4:-An unexpected error occurred}"
+# Utils
+_install_packages_pacman() {
+	toInstall=()
 
-	local spinner_frames=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
-
-	display_spinner() {
-		local pid=$!
-		local i=0
-		tput civis # Hide cursor
-
-		while kill -0 "${pid}" 2>/dev/null; do
-			printf "\r%s %s" "${spinner_frames[i]}" "${start_message}"
-			i=$(((i + 1) % ${#spinner_frames[@]}))
-			sleep 0.1
-		done
-
-		tput cnorm # Show cursor
-	}
-
-	# Run the task in the background, redirect stdout and stderr to separate temporary files
-	local temp_out_log=$(mktemp)
-	local temp_err_log=$(mktemp)
-	(${task} >"${temp_out_log}" 2>"${temp_err_log}") &
-
-	display_spinner "${start_message}"
-
-	# Wait for the task to complete
-	wait $!
-	local exit_code=$?
-
-	# Print last frame of the spinner
-	last_index=$((${#spinner_frames[@]} - 1))
-	printf "\r%s %s" "${spinner_frames[${last_index}]}" "${start_message}"
-
-	# Get the last echo message from the output log file
-	local last_echo=$(tail -n 1 "${temp_out_log}")
-
-	# Remove the last line of the output log file, if the exit code is greater than 1
-	if [[ ${exit_code} -gt 1 ]]; then
-		sed -i '$ d' "${temp_out_log}"
-	fi
-
-	# Display stdout logs if they are not empty
-	if [[ -s "${temp_out_log}" ]]; then
-		echo
-		cat "${temp_out_log}"
-	fi
-
-	# Display stderr logs if they are not empty
-	if [[ -s "${temp_err_log}" ]]; then
-		echo
-		cat "${temp_err_log}" | while IFS= read -r line; do echo -e "\033[31m${line}\033[0m"; done
-	fi
-
-	# Clear the line before displaying the final message
-	printf "\r\033[K"
-
-	# Display completion or error message based on exit code
-	if [[ ${exit_code} -eq 0 ]]; then
-		printf "\r\e[32m%s %s\e[0m\n" "󰗠" "${completion_message}" # Green color
-	elif [[ ${exit_code} -eq 1 ]]; then
-		printf "\r\e[31m%s %s\e[0m\n" "" "${error_message}" # Red color
-	else
-		printf "\r%s %s\n" "" "${last_echo}"
-	fi
-
-	# Clean up
-	rm "${temp_out_log}" "${temp_err_log}"
-}
-
-pacman_ensure_installed() {
-	local package_name="$1"
-	shift
-
-	install_with_pacman() {
-		if pacman -Qi "${package_name}" &>/dev/null; then
-			echo "${package_name}"
-			exit 2
+	for pkg; do
+		if pacman -Q "${pkg}" &>/dev/null; then
+			_log -l warn --prefix "pacman" "${pkg} is already installed"
+			continue
 		fi
+		toInstall+=("${pkg}")
+	done
 
-		sudo pacman -S --needed --noconfirm "${package_name}"
-	}
+	if [[ "${toInstall[*]}" == "" ]]; then
+		return
+	fi
 
-	show_spinner "install_with_pacman" "${package_name}" "${package_name}"
+	sudo pacman --needed --noconfirm -S "${toInstall[@]}"
 }
 
-paru_ensure_installed() {
-	local package_name="$1"
-	shift
+_install_packages_paru() {
+	toInstall=()
 
-	install_with_paru() {
-		if paru -Qi "${package_name}" &>/dev/null; then
-			echo "${package_name}"
-			exit 2
+	for pkg; do
+		if paru -Q "${pkg}" &>/dev/null; then
+			_log -l warn --prefix "paru" "${pkg} is already installed"
+			continue
 		fi
+		toInstall+=("${pkg}")
+	done
 
-		paru -S --needed --noconfirm "${package_name}"
-	}
+	if [[ "${toInstall[*]}" == "" ]]; then
+		return
+	fi
 
-	show_spinner "install_with_paru" "${package_name}" "${package_name}"
+	paru --needed -S "${toInstall[@]}"
 }
--- home/.chezmoitemplates/pwsh-library.ps1
+++ home/.chezmoitemplates/pwsh-library.ps1
@@ -2,110 +2,41 @@
 # │ POWERSHELL LIBRARY                                       │
 # ╰──────────────────────────────────────────────────────────╯
 
-function Write-LogError {
-  param([String]$Message)
-  Write-Host -Object " $Message" -ForegroundColor "Red"
-}
-function Write-StartTask {
-  param([String]$Message)
-  Write-Host -Object "󰪥 $Message"
-}
-function Write-CompleteTask {
-  param([String]$Message)
-  Write-Host -Object "󰗠 $Message" -ForegroundColor "Green"
-}
-function Write-LogInfo {
-  param([String]$Message)
-  Write-Host -Object " $Message"
-}
-function Invoke-CommandExpression {
-  param([String]$Message)
-  try {
-    Invoke-Expression $Message
-  }
-  catch {
-    throw "Command failed: $_"
-  }
+Write-Host
+
+function _Log {
+  gum log -s $Args
 }
 
-function Show-Spinner {
-  param (
-    [String]$ScriptBlock,
-    [String]$StartMessage,
-    [String]$CompletionMessage,
-    [Switch]$ShowLogs,
-    [String]$Prefix = ":"
-  )
-  [String[]]$SpinnerFrames = @("⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏")
+function _Spin {
+  param([String]$Title, [Parameter(Position = 1, ValueFromRemainingArguments = $true)]
+  $Rest)
 
-  $outputFile = [System.IO.Path]::GetTempFileName()
+  gum spin --show-error --spinner points --title $Title $Rest
+}
 
-  $Job = Start-Job -ScriptBlock {
-    param([String]$ScriptBlock, [String]$OutputFile)
-    try {
-      & ([ScriptBlock]::Create($ScriptBlock)) *>&1 | Out-File -FilePath $OutputFile -Append
+function _Install-PackagesWinget {
+  param([string[]]$Packages)
+
+  foreach ($Pkg in $Packages) {
+    if (winget list -e $Pkg | Out-Null) {
+      _Log -l warn --prefix "winget" "$Pkg is already installed"
+      continue
     }
-    catch {
-      $_ | Out-String | Out-File -FilePath $OutputFile -Append
-      throw
+
+    winget install -e --accept-package-agreements --accept-source-agreements --id $Pkg
+  }
+}
+
+function _Install-PackagesGallery {
+  param([string[]]$Packages)
+
+  foreach ($Pkg in $Packages) {
+    if (Get-Module -ListAvailable -Name $Pkg -ErrorAction SilentlyContinue) {
+      _Log -l warn --prefix "PowerShell Gallery" "$Pkg is already installed"
+      continue
     }
-  } -ArgumentList $ScriptBlock, $outputFile
 
-  $i = 0
-
-  while ($Job.State -eq "Running") {
-    Write-Host -NoNewline "`r$($SpinnerFrames[$i]) $StartMessage"
-    $i = ($i + 1) % $SpinnerFrames.Count
-    Start-Sleep -Milliseconds 100
+    Install-Module -AcceptLicense -Name $Pkg
   }
-
-  # Display last spinner frame
-  Write-Host -NoNewline "`r$($SpinnerFrames[-1]) $StartMessage"
-
-  # Read job output from the file
-  $JobOutput = Get-Content -Path $outputFile
-
-  # Display logs or errors
-  $HasError = $Job.ChildJobs[0].JobStateInfo.State -eq "Failed"
-  if ($ShowLogs -or $HasError) {
-    Write-Host
-    $IsError = $false
-    foreach ($line in $JobOutput) {
-      if ($line -match "Exception:") {
-        $IsError = $true
-      }
-      if ($IsError) {
-        Write-Host -ForegroundColor Red -Object $line
-      }
-      else {
-        Write-Host -Object $line
-      }
-    }
-  }
-
-  # Check for controlled exit
-  $HasControlledExit = $false
-  $ControlledExitMessage = $null
-  foreach ($line in $JobOutput) {
-    if ($line -is [string] -and $line.StartsWith($Prefix)) {
-      $HasControlledExit = $true
-      $ControlledExitMessage = $line.Substring($Prefix.Length).Trim()
-      break
-    }
-  }
-
-  $Padding = " " * [Console]::WindowWidth
-  Write-Host -NoNewline "`r$Padding"
-  if ($HasError) {
-    Write-Host -ForegroundColor Red -Object "`r An unexpected error occurred"
-  }
-  elseif ($HasControlledExit) {
-    Write-Host -Object "`r $ControlledExitMessage"
-  }
-  else {
-    Write-Host -ForegroundColor "Green" -Object "`r󰗠 $CompletionMessage"
-  }
-
-  $Job | Remove-Job -Force
-  Remove-Item -Path $outputFile -Force
 }
