a888ff973f043b318897e50505e6c33c678aaf7d
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-25 13:51:28 +0200

    feat(scripts): add whitespace and quote support for `_set_option_with_sudo`

--- home/.chezmoitemplates/bash-library.sh
+++ home/.chezmoitemplates/bash-library.sh
@@ -22,11 +22,24 @@ _spin() {
 # ── Utils ─────────────────────────────────────────────────────────────
 
 # Function to set an option with sudo access
-# Usage: _set_option_with_sudo <option_name> <option_value> <config_file>
+# Usage: _set_option_with_sudo <option_name> <option_value> <config_file> <optional: quote_value> <optional: add_whitespace>
 _set_option_with_sudo() {
 	local option_name="$1"
 	local option_value="$2"
 	local config_file="$3"
+	local quote_value="${4:-false}"
+	local add_whitespace="${5:-false}"
+
+	# Quote the option value if the quote_value flag is set to true
+	if [[ "${quote_value}" = true ]]; then
+		option_value="\"${option_value}\""
+	fi
+
+	# Define the whitespace around the equal sign if add_whitespace is set to true
+	local whitespace=""
+	if [[ "${add_whitespace}" = true ]]; then
+		whitespace=" "
+	fi
 
 	# Regular expression patterns to match lines with optional whitespace around the equal sign
 	local pattern="^\s*${option_name}\s*=\s*${option_value}\s*$"
@@ -41,18 +54,18 @@ _set_option_with_sudo() {
 
 	# Check if the option is commented out with the exact value
 	if grep -Eq "${commented_pattern}" "${config_file}"; then
-		sudo sed -i "s|${commented_pattern}|${option_name}=${option_value}|" "${config_file}"
+		sudo sed -i "s|${commented_pattern}|${option_name}${whitespace}=${whitespace}${option_value}|" "${config_file}"
 	else
 		# Check if the option is commented out with any value
 		if grep -Eq "${commented_any_value_pattern}" "${config_file}"; then
-			sudo sed -i "s|${commented_any_value_pattern}|${option_name}=${option_value}|" "${config_file}"
+			sudo sed -i "s|${commented_any_value_pattern}|${option_name}${whitespace}=${whitespace}${option_value}|" "${config_file}"
 		else
 			# Check if the option is set with any value
 			if grep -Eq "${any_value_pattern}" "${config_file}"; then
-				sudo sed -i "s|${any_value_pattern}|${option_name}=${option_value}|" "${config_file}"
+				sudo sed -i "s|${any_value_pattern}|${option_name}${whitespace}=${whitespace}${option_value}|" "${config_file}"
 			else
 				# If the option is not present at all, add it to the end of the config file
-				echo "${option_name}=${option_value}" | sudo tee -a "${config_file}"
+				echo "${option_name}${whitespace}=${whitespace}${option_value}" | sudo tee -a "${config_file}"
 			fi
 		fi
 	fi
