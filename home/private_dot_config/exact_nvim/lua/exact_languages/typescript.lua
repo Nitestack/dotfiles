183b615b5540a00ec411787b1333b41b4a5f38e4
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-27 01:28:00 +0200

    feat(nvim): disable `eslint`

--- home/private_dot_config/exact_nvim/lua/exact_languages/typescript.lua
+++ home/private_dot_config/exact_nvim/lua/exact_languages/typescript.lua
@@ -52,6 +52,10 @@ return utils.plugin.get_language_spec({
           },
         },
       },
+      eslint = {
+        autostart = false,
+        mason = false,
+      },
     },
   },
   mason = {
@@ -73,10 +77,10 @@ return utils.plugin.get_language_spec({
   },
   linter = {
     linters_by_ft = {
-      ["javascript"] = { "eslint_d", "eslint" },
-      ["javascriptreact"] = { "eslint_d", "eslint" },
-      ["typescript"] = { "eslint_d", "eslint" },
-      ["typescriptreact"] = { "eslint_d", "eslint" },
+      ["javascript"] = { "eslint_d" },
+      ["javascriptreact"] = { "eslint_d" },
+      ["typescript"] = { "eslint_d" },
+      ["typescriptreact"] = { "eslint_d" },
     },
   },
   plugins = utils.plugin.with_extensions({
