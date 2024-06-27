5ea9feb62cb2106d0d02eab9c120e46cbf61016b
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-27 11:40:06 +0200

    refactor(ags): remove matugen

--- home/private_dot_config/exact_ags/lib/init.ts
+++ home/private_dot_config/exact_ags/lib/init.ts
@@ -2,7 +2,6 @@ import lowBattery from "./battery";
 import colorgen from "./colorgen";
 import gtk from "./gtk";
 import hyprland from "./hyprland";
-import matugen from "./matugen";
 import notifications from "./notifications";
 import tmux from "./tmux";
 
@@ -10,7 +9,6 @@ export default function init() {
   try {
     gtk();
     tmux();
-    matugen();
     colorgen();
     hyprland();
     lowBattery();
