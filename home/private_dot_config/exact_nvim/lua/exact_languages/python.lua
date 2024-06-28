2db2d527f6dfb283e9b4510d6fcf55dc0271effc
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-06-28 15:09:00 +0200

    feat(nvim): add Python support

---
+++ home/private_dot_config/exact_nvim/lua/exact_languages/python.lua
@@ -0,0 +1,9 @@
+-- ╭─────────────────────────────────────────────────────────╮
+-- │ Python                                                  │
+-- ╰─────────────────────────────────────────────────────────╯
+
+return utils.plugin.get_language_spec({
+  plugins = {
+    { import = "lazyvim.plugins.extras.lang.python" },
+  },
+})
