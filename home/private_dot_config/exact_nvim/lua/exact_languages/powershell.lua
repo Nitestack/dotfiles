dd7955a79abe58ff36f6e944aa92c4f1361a3f5c
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-07-06 14:30:54 +0200

    feat(nvim): only install `powershell-es` on Windows

--- home/private_dot_config/exact_nvim/lua/exact_languages/powershell.lua
+++ home/private_dot_config/exact_nvim/lua/exact_languages/powershell.lua
@@ -2,13 +2,15 @@
 -- │ PowerShell                                              │
 -- ╰─────────────────────────────────────────────────────────╯
 
-return utils.plugin.get_language_spec({
-  mason = {
-    "powershell-editor-services",
-  },
-  lsp = {
-    servers = {
-      powershell_es = {},
-    },
-  },
-})
+return utils.is_win()
+    and utils.plugin.get_language_spec({
+      mason = {
+        "powershell-editor-services",
+      },
+      lsp = {
+        servers = {
+          powershell_es = {},
+        },
+      },
+    })
+  or {}
