0c37b01a4b11dcfdf61426207cbf91156725ceec
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-07-13 20:29:44 +0200

    feat(arch): change icon theme to `WhiteSur`

--- home/private_dot_config/exact_ags/lib/gtk.ts
+++ home/private_dot_config/exact_ags/lib/gtk.ts
@@ -10,9 +10,8 @@ function gtk() {
   const scheme = options.theme.scheme.value;
   const suffixLower = options.theme.scheme.value === "dark" ? "-dark" : "";
   const suffixUpper = options.theme.scheme.value === "dark" ? "-Dark" : "";
-  settings.set_string("color-scheme", `prefer-${scheme}`);
+  settings.set_string("color-scheme", `prefer${suffixLower}`);
   settings.set_string("gtk-theme", `adw-gtk3${suffixLower}`);
-  settings.set_string("icon-theme", `${ICON_THEME}${suffixUpper}`);
 }
 
 export default function init() {
