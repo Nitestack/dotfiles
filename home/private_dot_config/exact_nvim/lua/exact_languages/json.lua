6aef733c69314a88580747d17bf04bd0a53bd099
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-07-14 12:45:57 +0200

    feat(nvim): remove `jsonlint`

--- home/private_dot_config/exact_nvim/lua/exact_languages/json.lua
+++ home/private_dot_config/exact_nvim/lua/exact_languages/json.lua
@@ -6,14 +6,6 @@ return utils.plugin.get_language_spec({
   mason = {
     "prettierd",
     "prettier",
-    "jsonlint",
-    "json-lsp",
-  },
-  linter = {
-    linters_by_ft = {
-      ["json"] = { "jsonlint" },
-      ["jsonc"] = { "jsonlint" },
-    },
   },
   formatter = {
     formatters_by_ft = {
