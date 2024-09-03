5a7e7369c3fcb8804186cf16ccafbc5db3abaec1
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-09-03 20:38:17 +0200

    feat(nix): use GRUB instead of systemd

--- nix/nixos/system.nix
+++ nix/nixos/system.nix
@@ -148,9 +148,21 @@
   boot = {
     tmp.cleanOnBoot = true;
     loader = {
-      timeout = 2;
-      systemd-boot.enable = true;
+      timeout = 60;
       efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        theme = (
+          pkgs.sleek-grub-theme.override {
+            withStyle = "dark";
+            withBanner = "Boot Manager";
+          }
+        );
+        efiSupport = true;
+        useOSProber = true;
+        device = "nodev";
+        gfxmodeEfi = "1920x1080,auto";
+      };
     };
   };
 
