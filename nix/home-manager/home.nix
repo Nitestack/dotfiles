ce653c4d6922cdcc1eb208db8c710a25305a7842
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-09-05 22:59:34 +0200

    feat(nix): add desktop entry

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -2,7 +2,7 @@
 # │ HOME MANAGER CONFIGURATION                               │
 # ╰──────────────────────────────────────────────────────────╯
 
-{ meta, ... }:
+{ pkgs, meta, ... }:
 {
   # ── Imports ───────────────────────────────────────────────────────────
   imports = [
@@ -20,6 +20,29 @@
 
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "24.05";
+  };
+
+  xdg = {
+    enable = true;
+    desktopEntries = {
+      snapdrop = {
+        name = "Snapdrop";
+        comment = "The easiest way to transfer files across devices";
+        categories = [
+          "Network"
+          "FileTransfer"
+        ];
+        exec = "google-chrome-stable --app=https://snapdrop.net --name=Snapdrop";
+        icon = "${pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/snapdrop/snapdrop/master/client/images/logo_transparent_512x512.png";
+          sha256 = "sha256-QMgXdeaNxg+e71dKAojR1h1zcpwBCNX10JQfD0fqhes=";
+        }}";
+      };
+    };
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
   };
 
   # ── Programs ──────────────────────────────────────────────────────────
