98f295545d86da6b8718695115ea15e37a57f627
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-09-15 19:57:20 +0200

    refactor(nix): move nix flake update script to Home Manager config

--- nix/home-manager/scripts/nix-utils.nix
+++ nix/home-manager/scripts/nix-utils.nix
@@ -12,13 +12,21 @@ let
   homeDir = config.home.homeDirectory;
 
   nix-switch = pkgs.writeShellScriptBin "nix-switch" ''
+    #!/usr/bin/env bash
     sudo ${nixos-rebuild} switch --flake ${homeDir}/.dotfiles/nix#${meta.hostname} --impure $@
   '';
   nix-boot = pkgs.writeShellScriptBin "nix-boot" ''
+    #!/usr/bin/env bash
     sudo ${nixos-rebuild} boot --flake ${homeDir}/.dotfiles/nix#${meta.hostname} --impure $@
   '';
   nix-test = pkgs.writeShellScriptBin "nix-test" ''
+    #!/usr/bin/env bash
     sudo ${nixos-rebuild} test --flake ${homeDir}/.dotfiles/nix#${meta.hostname} --impure $@
+  '';
+
+  nix-flake-update = pkgs.writeShellScriptBin "nix-flake-update" ''
+    #!/usr/bin/env bash
+    nix flake update --commit-lock-file ${homeDir}/.dotfiles/nix $@
   '';
 in
 {
@@ -26,5 +34,6 @@ in
     nix-switch
     nix-boot
     nix-test
+    nix-flake-update
   ];
 }
