02c13d92a5e97b973edf15fede8e0bea284ed829
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-10-02 12:06:49 +0200

    feat(nvim): add `typstfmt`

--- config/private_dot_config/exact_nvim/lua/exact_languages/typst.lua
+++ config/private_dot_config/exact_nvim/lua/exact_languages/typst.lua
@@ -1,9 +1,14 @@
 return utils.plugin.get_language_spec({
   treesitter = "typst",
-  mason = "typst-lsp",
+  mason = { "typst-lsp", "typstfmt" },
   lsp = {
     servers = {
       typst_lsp = {},
+    },
+  },
+  formatter = {
+    formatters_by_ft = {
+      ["typst"] = { "typstfmt" },
     },
   },
   plugins = {
