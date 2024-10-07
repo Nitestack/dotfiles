592cfb6545d93dfc25d358eb6cb22e9513100a18
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-10-07 22:33:48 +0200

    feat(nix): change hypridle times

--- nix/home-manager/hyprland/hypridle.nix
+++ nix/home-manager/hyprland/hypridle.nix
@@ -22,27 +22,27 @@ in
       listener = [
         {
           timeout = 150; # 2.5min
-          on-timeout = "${brightnessctl} -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor
-          on-resume = "${brightnessctl} -r"; # monitor backlight restore
-        }
-        {
-          timeout = 150; # 2.5min
           on-timeout = "${brightnessctl} -sd dell::kbd_backlight set 0"; # turn off keyboard backlight
           on-resume = "${brightnessctl} -rd dell::kbd_backlight"; # turn on keyboard backlight
         }
         {
-          timeout = 300; # 5min
-          on-timeout = "${loginctl} lock-session"; # lock screen when timeout has passed
+          timeout = 150; # 2.5min
+          on-timeout = "${brightnessctl} -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor
+          on-resume = "${brightnessctl} -r"; # monitor backlight restore
         }
         {
-          timeout = 330; # 5.5min
+          timeout = 300; # 5min
           on-timeout = "${hyprctl} dispatch dpms off"; # screen off when timeout has passed
           on-resume = "${hyprctl} dispatch dpms on"; # screen on when activity is detected after timeout has fired
         }
         {
-          timeout = 1800; # 30min
-          on-timeout = "${systemctl} suspend"; # suspend pc
+          timeout = 600; # 10min
+          on-timeout = "${loginctl} lock-session"; # lock screen when timeout has passed
         }
+        # {
+        #   timeout = 1800; # 30min
+        #   on-timeout = "${systemctl} suspend"; # suspend pc
+        # }
       ];
     };
   };
