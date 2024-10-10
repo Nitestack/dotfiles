17ec3c7a01d2d20752b67265d7bc454993eab808
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-10-10 22:37:13 +0200

    fix(wezterm): don't use duplicate configs and check for tmux

--- config/private_dot_config/wezterm/wezterm.lua
+++ config/private_dot_config/wezterm/wezterm.lua
@@ -6,20 +6,30 @@
 ---@diagnostic disable-next-line: assign-type-mismatch
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 
 require("config.appearance").setup(config)
 require("config.general").setup(config)
 require("config.keys").setup(config)
 require("config.platforms").setup(config)
 
----@type SmartSplitsWeztermConfig
-local smart_splits_config = {
-  direction_keys = { "h", "j", "k", "l" },
-  modifiers = {
-    move = "CTRL",
-    resize = "LEADER",
-  },
-}
+local function is_tmux_available()
+  local success, stdout = wezterm.run_child_process({ "which", "tmux" })
+  return success and stdout ~= nil and stdout ~= "" -- If `which tmux` succeeds and returns a valid path
+end
 
-return smart_splits.apply_to_config(config, smart_splits_config)
+if is_tmux_available() then
+  return config
+else
+  local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+
+  ---@type SmartSplitsWeztermConfig
+  local smart_splits_config = {
+    direction_keys = { "h", "j", "k", "l" },
+    modifiers = {
+      move = "CTRL",
+      resize = "LEADER",
+    },
+  }
+
+  return smart_splits.apply_to_config(config, smart_splits_config)
+end
--- nix/home-manager/wezterm.nix
+++ nix/home-manager/wezterm.nix
@@ -7,24 +7,7 @@
     enable = true;
     package = inputs.wezterm.packages.${pkgs.system}.default;
     enableZshIntegration = true;
-    extraConfig = ''
-      local config = wezterm.config_builder()
-      local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-
-      require("config.appearance").setup(config)
-      require("config.general").setup(config)
-      require("config.keys").setup(config)
-      require("config.platforms").setup(config)
-
-      local smart_splits_config = {
-        direction_keys = { "h", "j", "k", "l" },
-        modifiers = {
-          move = "CTRL",
-          resize = "LEADER",
-        },
-      }
-
-      return smart_splits.apply_to_config(config, smart_splits_config)
-    '';
   };
+  # handled by chezmoi
+  xdg.configFile."wezterm/wezterm.lua".enable = false;
 }
