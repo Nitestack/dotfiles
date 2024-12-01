58240677cbcbe61bcdedadcc0e6d159aa78cf947
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-12-01 14:15:23 +0100

    feat(nvim): add `smear-cursor.nvim`

--- config/private_dot_config/exact_nvim/lua/exact_plugins/_extras.lua
+++ config/private_dot_config/exact_nvim/lua/exact_plugins/_extras.lua
@@ -6,6 +6,7 @@
 ---@type LazySpec
 return {
   { import = "lazyvim.plugins.extras.coding.neogen" },
+  { import = "lazyvim.plugins.extras.ui.smear-cursor" },
   { import = "lazyvim.plugins.extras.util.dot" },
   { import = "lazyvim.plugins.extras.vscode" },
 }
