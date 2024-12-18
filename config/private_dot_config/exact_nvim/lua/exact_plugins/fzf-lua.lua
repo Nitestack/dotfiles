b14449d4de9e6efc08d7bf793c21297d023d210a
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-12-18 21:23:50 +0100

    feat(nvim): use default fzf settings

--- config/private_dot_config/exact_nvim/lua/exact_plugins/fzf-lua.lua
+++ config/private_dot_config/exact_nvim/lua/exact_plugins/fzf-lua.lua
@@ -1,19 +1,4 @@
-return utils.plugin.with_extensions({
-  { import = "lazyvim.plugins.extras.editor.fzf" },
-  {
-    "ibhagwan/fzf-lua",
-    opts = function(_, opts)
-      -- Win options
-      opts.winopts = opts.winopts or {}
-      opts.winopts.preview = opts.winopts.preview or {}
-      opts.winopts.preview.layout = "vertical"
-
-      -- Files
-      opts.files = opts.files or {}
-      opts.files.previewer = false
-    end,
-  },
-}, {
+return utils.plugin.with_extensions({}, {
   lualine = "fzf",
   catppuccin = {
     fzf = true,
