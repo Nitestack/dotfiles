23f20930ca9c9373c64d532ebaa823e0147689a7
Nitestack <74626967+Nitestack@users.noreply.github.com>
2024-12-30 01:03:26 +0100

    fix(nix): set Hyprland as the default session

--- nix/nixos/sddm.nix
+++ nix/nixos/sddm.nix
@@ -1,16 +1,19 @@
 # ╭──────────────────────────────────────────────────────────╮
-# │ Display Manager                                          │
+# │ SDDM                                                     │
 # ╰──────────────────────────────────────────────────────────╯
 { pkgs, meta, ... }:
 let
   inherit (meta) font;
 in
 {
-  services.displayManager.sddm = {
-    enable = true;
-    package = pkgs.kdePackages.sddm;
-    theme = "catppuccin-mocha";
-    wayland.enable = true;
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      package = pkgs.kdePackages.sddm;
+      theme = "catppuccin-mocha";
+      wayland.enable = true;
+    };
+    defaultSession = "hyprland";
   };
   environment.systemPackages = [
     (pkgs.catppuccin-sddm.override {
