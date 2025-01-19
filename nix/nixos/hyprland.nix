e1452bcd532e422a6bc890c384ee86f605adbdc3
Nitestack <74626967+Nitestack@users.noreply.github.com>
2025-01-19 23:22:34 +0100

    feat(nix): edit desktop portal config

--- nix/nixos/hyprland.nix
+++ nix/nixos/hyprland.nix
@@ -8,7 +8,20 @@
     hyprlock.enable = true;
   };
 
-  xdg.portal.enable = true;
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      hyprland.default = [
+        "gtk"
+        "hyprland"
+      ];
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
 
   security = {
     pam.services.hyprlock = { };
