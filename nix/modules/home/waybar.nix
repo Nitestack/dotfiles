ee18593707a75fdd8a9d18dce48455670d5ad21d
Nitestack <74626967+Nitestack@users.noreply.github.com>
2025-04-22 23:38:36 +0200

    feat(waybar): add network group

--- nix/modules/home/waybar.nix
+++ nix/modules/home/waybar.nix
@@ -36,167 +36,199 @@ in
       enable = true;
       target = "hyprland-session.target";
     };
-    settings = [
-      {
-        layer = "top";
-        position = "top";
-        # Modules
-        modules-left = [
-          "custom/logo"
-          "hyprland/window"
-          "mpris"
-        ];
-        modules-center = [
-          "hyprland/workspaces"
-        ];
-        modules-right = [
-          "tray"
-          "privacy"
-          "wireplumber"
-          "clock"
-          "group/group-power"
-        ];
-        "custom/logo" = {
-          format = "";
-          tooltip = false;
-          on-click = "pkill rofi || ${rofi} -show drun";
-        };
-        "hyprland/window" = {
-          format = "{class}";
-          separate-outputs = true;
-          rewrite = {
-            ".blueman-manager-wrapped" = "Bluetooth";
-            "com.mitchellh.ghostty" = "Ghostty";
-            "org.gnome.Calculator" = "Calculator";
-            "org.gnome.Calendar" = "Calendar";
-            "org.gnome.clocks" = "Clocks";
-            "dconf-editor" = "dconf Editor";
-            "vesktop" = "Discord";
-            "gnome-disks" = "Disks";
-            "evince" = "Document Viewer";
-            "com.github.wwmm.easyeffects" = "Easy Effects";
-            "org.gnome.FileRoller" = "Archive Manager";
-            "org.gnome.Nautilus" = "Files";
-            "org.gnome.font-view" = "Fonts";
-            "google-chrome" = "Google Chrome";
-            "org.gnome.Loupe" = "Image Viewer";
-            "org.gnome.Music" = "Music Player";
-            "ONLYOFFICE" = "Document Editor";
-            ".protonvpn-app-wrapped" = "Proton VPN";
-            "qemu" = "QEMU";
-            "qt(.*)ct" = "Qt $1 Configuration Tool";
-            "rygel-preferences" = "UPnP/DLNA Preferences";
-            "Safeeyes" = "Safe Eyes";
-            "org.gnome.Settings" = "Settings";
-            "signal" = "Signal";
-            "spotify" = "Spotify";
-            "steam" = "Steam";
-            "com.stremio.stremio" = "Stremio";
-            "com.gnome.SystemMonitor" = "System Monitor";
-            "org.gnome.Todo" = "Todo";
-            "totem" = "Video Player";
-            ".virt-manager-wrapped" = "Virtual Machine Manager";
-            "code" = "Visual Studio Code";
-            "wasistlos" = "WhatsApp";
-            "org.wezfurlong.wezterm" = "WezTerm";
-            "zen" = "Zen Browser";
-            "" = "Desktop";
-          };
-        };
-        privacy = {
-          icon-size = 16;
-          icon-spacing = 12;
-        };
-        mpris = {
-          format = "{player_icon} {dynamic}";
-          format-paused = "{status_icon} {dynamic}";
-          interval = 1;
-          dynamic-len = 60;
-          dynamic-order = [
-            "title"
-            "artist"
+    settings =
+      let
+        spacing = 12;
+      in
+      [
+        {
+          layer = "top";
+          position = "top";
+          # Modules
+          modules-left = [
+            "custom/logo"
+            "hyprland/window"
+            "mpris"
           ];
-          player-icons = {
-            firefox = "";
-            spotify = "";
-            default = "";
-          };
-          status-icons = {
-            playing = "";
-            paused = "";
-            stopped = "";
-          };
-        };
-        "hyprland/workspaces" = {
-          format = " {icon} ";
-          format-icons = {
-            active = "";
-            default = "";
-          };
-          on-scroll-up = "${hyprctl} dispatch workspace e+1";
-          on-scroll-down = "${hyprctl} dispatch workspace e-1";
-        };
-        tray = {
-          icon-size = 18;
-          spacing = 12;
-        };
-        wireplumber = {
-          format = "{icon} {volume}%";
-          format-muted = " {volume}%";
-          format-icons = [
-            ""
-            ""
-            " "
+          modules-center = [
+            "hyprland/workspaces"
           ];
-          on-click = "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-        };
-        clock = {
-          format = "{:%a %b %e %R}";
-          tooltip-format = "{calendar}";
-          calendar.format = {
-            months = "<big><span color='${textColor}' face='${font.sans.name}'><b>{}</b></span></big>";
-            days = "<tt><span color='${textColor}'>{}</span></tt>";
-            weekdays = "<tt><span color='${errorColor}'>{}</span></tt>";
-            today = "<tt><span color='${primaryColor}'><b>{}</b></span></tt>";
-          };
-          actions = {
-            on-click = "shift_reset";
-            on-scroll-up = "shift_down";
-            on-scroll-down = "shift_up";
-          };
-        };
-        "group/group-power" = {
-          orientation = "inherit";
-          drawer.transition-left-to-right = false;
-          modules = [
-            "custom/shutdown"
-            "custom/lock"
-            "custom/suspend"
-            "custom/reboot"
+          modules-right = [
+            "tray"
+            "privacy"
+            "wireplumber"
+            "group/group-network"
+            "clock"
+            "group/group-power"
           ];
-        };
-        "custom/shutdown" = {
-          tooltip = false;
-          on-click = "systemctl poweroff";
-          format = "";
-        };
-        "custom/lock" = {
-          tooltip = false;
-          on-click = "loginctl lock-session";
-          format = "";
-        };
-        "custom/suspend" = {
-          format = "";
-          tooltip = false;
-          on-click = "systemctl suspend";
-        };
-        "custom/reboot" = {
-          format = "";
-          tooltip = false;
-          on-click = "systemctl reboot";
-        };
-      }
-    ];
+          "custom/logo" = {
+            format = "";
+            tooltip = false;
+            on-click = "pkill rofi || ${rofi} -show drun";
+          };
+          "hyprland/window" = {
+            format = "{class}";
+            separate-outputs = true;
+            rewrite = {
+              ".blueman-manager-wrapped" = "Bluetooth";
+              "com.mitchellh.ghostty" = "Ghostty";
+              "org.gnome.Calculator" = "Calculator";
+              "org.gnome.Calendar" = "Calendar";
+              "org.gnome.clocks" = "Clocks";
+              "dconf-editor" = "dconf Editor";
+              "vesktop" = "Discord";
+              "gnome-disks" = "Disks";
+              "evince" = "Document Viewer";
+              "com.github.wwmm.easyeffects" = "Easy Effects";
+              "org.gnome.FileRoller" = "Archive Manager";
+              "org.gnome.Nautilus" = "Files";
+              "org.gnome.font-view" = "Fonts";
+              "google-chrome" = "Google Chrome";
+              "org.gnome.Loupe" = "Image Viewer";
+              "org.gnome.Music" = "Music Player";
+              "ONLYOFFICE" = "Document Editor";
+              ".protonvpn-app-wrapped" = "Proton VPN";
+              "qemu" = "QEMU";
+              "qt(.*)ct" = "Qt $1 Configuration Tool";
+              "rygel-preferences" = "UPnP/DLNA Preferences";
+              "Safeeyes" = "Safe Eyes";
+              "org.gnome.Settings" = "Settings";
+              "signal" = "Signal";
+              "spotify" = "Spotify";
+              "steam" = "Steam";
+              "com.stremio.stremio" = "Stremio";
+              "com.gnome.SystemMonitor" = "System Monitor";
+              "org.gnome.Todo" = "Todo";
+              "totem" = "Video Player";
+              ".virt-manager-wrapped" = "Virtual Machine Manager";
+              "code" = "Visual Studio Code";
+              "wasistlos" = "WhatsApp";
+              "org.wezfurlong.wezterm" = "WezTerm";
+              "zen" = "Zen Browser";
+              "" = "Desktop";
+            };
+          };
+          privacy = {
+            icon-spacing = spacing;
+            icon-size = 16;
+          };
+          mpris = {
+            format = "{player_icon} {dynamic}";
+            format-paused = "{status_icon} {dynamic}";
+            interval = 1;
+            dynamic-len = 60;
+            dynamic-order = [
+              "title"
+              "artist"
+            ];
+            player-icons = {
+              firefox = "";
+              spotify = "";
+              default = "";
+            };
+            status-icons = {
+              playing = "";
+              paused = "";
+              stopped = "";
+            };
+          };
+          "hyprland/workspaces" = {
+            format = " {icon} ";
+            format-icons = {
+              active = "";
+              default = "";
+            };
+            on-scroll-up = "${hyprctl} dispatch workspace e+1";
+            on-scroll-down = "${hyprctl} dispatch workspace e-1";
+          };
+          tray = {
+            inherit spacing;
+            icon-size = 18;
+          };
+          wireplumber = {
+            format = "{icon} {volume}%";
+            format-muted = " {volume}%";
+            format-icons = [
+              ""
+              ""
+              " "
+            ];
+            on-click = "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          };
+          "group/group-network" = {
+            orientation = "inherit";
+            drawer.click-to-reveal = true;
+            modules = [
+              "network"
+              "network#download"
+              "network#upload"
+            ];
+          };
+          network = {
+            format-ethernet = "󰈀";
+            format-wifi = "󰖩";
+            format-disconnected = "󰖪";
+            tooltip-format = "{ifname}\n{gwaddr}";
+            tooltip-format-wifi = "{essid} ({frequency} GHz)\n{gwaddr}";
+            tooltip-format-disconnected = "Disconnected";
+          };
+          "network#download" = {
+            interval = 1;
+            format = "󰇚 {bandwidthDownBits}";
+            tooltip-format = "Receiving";
+          };
+          "network#upload" = {
+            interval = 1;
+            format = "󰕒 {bandwidthUpBits}";
+            tooltip-format = "Sending";
+          };
+          clock = {
+            format = "{:%a %b %e %R}";
+            tooltip-format = "{calendar}";
+            calendar.format = {
+              months = "<big><span color='${textColor}' face='${font.sans.name}'><b>{}</b></span></big>";
+              days = "<tt><span color='${textColor}'>{}</span></tt>";
+              weekdays = "<tt><span color='${errorColor}'>{}</span></tt>";
+              today = "<tt><span color='${primaryColor}'><b>{}</b></span></tt>";
+            };
+            actions = {
+              on-click = "shift_reset";
+              on-scroll-up = "shift_down";
+              on-scroll-down = "shift_up";
+            };
+          };
+          "group/group-power" = {
+            orientation = "inherit";
+            drawer.transition-left-to-right = false;
+            modules = [
+              "custom/shutdown"
+              "custom/lock"
+              "custom/suspend"
+              "custom/reboot"
+            ];
+          };
+          "custom/shutdown" = {
+            tooltip = false;
+            on-click = "systemctl poweroff";
+            format = "";
+          };
+          "custom/lock" = {
+            tooltip = false;
+            on-click = "loginctl lock-session";
+            format = "";
+          };
+          "custom/suspend" = {
+            format = "";
+            tooltip = false;
+            on-click = "systemctl suspend";
+          };
+          "custom/reboot" = {
+            format = "";
+            tooltip = false;
+            on-click = "systemctl reboot";
+          };
+        }
+      ];
     style = ''
       * {
         font-family: "${font.sans.name}", "${font.nerd.propoName}";
@@ -220,9 +252,23 @@ in
       }
 
       /* Modules with Background */
-      #mpris, #workspaces, #tray, #privacy, #group-power:hover {
+      #mpris, #workspaces, #tray, #privacy, #group-network:hover, #group-power:hover {
         border-radius: 1rem;
         background-color: ${secondaryColor};
+      }
+
+      /*
+      * ── Group Styles ──────────────────────────────────────────────────────
+      */
+
+      /* Left Items */
+      #network, #custom-lock {
+        margin-left: 0.5rem;
+      }
+
+      /* All Modules */
+      #network, network.download, network.upload, #custom-shutdown, #custom-lock, #custom-suspend, #custom-reboot {
+        background-color: transparent;
       }
 
       /*
@@ -284,12 +330,13 @@ in
         color: ${errorColor};
       }
 
-      /* Power Group */
-      #custom-shutdown, #custom-lock, #custom-suspend, #custom-reboot {
-        background-color: transparent;
+      /* Network Group */
+      #network.disconnected {
+        color: ${errorColor};
       }
+
+      /* Power Group */
       #custom-lock {
-        margin-left: 0.5rem;
         color: ${primaryColor};
       }
       #custom-suspend {
