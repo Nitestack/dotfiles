04410b1746d4af6451492b5b36105ffb823ccb05
Nitestack <74626967+Nitestack@users.noreply.github.com>
2025-06-28 18:16:00 +0200

    feat(nix): switch to systemd-boot

--- nix/modules/nixos/boot.nix
+++ nix/modules/nixos/boot.nix
@@ -1,26 +1,13 @@
 # ╭──────────────────────────────────────────────────────────╮
 # │ Boot                                                     │
 # ╰──────────────────────────────────────────────────────────╯
-{ pkgs, ... }:
 {
   boot = {
     tmp.cleanOnBoot = true;
     loader = {
       timeout = 60;
       efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        theme = (
-          pkgs.sleek-grub-theme.override {
-            withStyle = "dark";
-            withBanner = "Boot Manager";
-          }
-        );
-        efiSupport = true;
-        useOSProber = true;
-        device = "nodev";
-        gfxmodeEfi = "1920x1080,auto";
-      };
+      systemd-boot.enable = true;
     };
   };
 }
