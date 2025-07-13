d34184458bb7a705be963c1c21fe898fdf3e7a65
Nitestack <code@npham.de>
2025-07-13 02:50:43 +0200

    feat(hyprland): update animations

--- nix/modules/home/hyprland/animations.nix
+++ nix/modules/home/hyprland/animations.nix
@@ -3,46 +3,34 @@
 # ╰──────────────────────────────────────────────────────────╯
 {
   wayland.windowManager.hyprland.settings.animations = {
-    enabled = true; # enable animations
-
-    # https://wiki.hyprland.org/Configuring/Animations
+    enabled = true;
     bezier = [
-      "linear, 0, 0, 1, 1"
-      "md3_standard, 0.2, 0, 0, 1"
-      "md3_decel, 0.05, 0.7, 0.1, 1"
-      "md3_accel, 0.3, 0, 0.8, 0.15"
-      "overshot, 0.05, 0.9, 0.1, 1.1"
-      "crazyshot, 0.1, 1.5, 0.76, 0.92 "
-      "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+      "expressiveFastSpatial, 0.42, 1.67, 0.21, 0.90"
+      "expressiveSlowSpatial, 0.39, 1.29, 0.35, 0.98"
+      "expressiveDefaultSpatial, 0.38, 1.21, 0.22, 1.00"
+      "emphasizedDecel, 0.05, 0.7, 0.1, 1"
+      "emphasizedAccel, 0.3, 0, 0.8, 0.15"
+      "standardDecel, 0, 0, 0, 1"
       "menu_decel, 0.1, 1, 0, 1"
-      "menu_accel, 0.38, 0.04, 1, 0.07"
-      "easeInOutCirc, 0.85, 0, 0.15, 1"
-      "easeOutCirc, 0, 0.55, 0.45, 1"
-      "easeOutExpo, 0.16, 1, 0.3, 1"
-      "softAcDecel, 0.26, 0.26, 0.15, 1"
-      "md2, 0.4, 0, 0.2, 1 # use with .2s duration"
+      "menu_accel, 0.52, 0.03, 0.72, 0.08"
     ];
     animation = [
-      # Windows
-      "windows, 1, 3, md3_decel, popin 60%"
-      "windowsIn, 1, 3, md3_decel, popin 60%"
-      "windowsOut, 1, 3, md3_accel, popin 60%"
-
-      # Layers
-      "layersIn, 1, 3, menu_decel, slide"
-      "layersOut, 1, 1.6, menu_accel"
-
-      # Fade
-      "fade, 1, 3, md3_decel"
-      "fadeLayersIn, 1, 2, menu_decel"
-      "fadeLayersOut, 1, 4.5, menu_accel"
-
-      # Border
-      "border, 1, 10, default"
-
-      # Workspaces
+      # windows
+      "windowsIn, 1, 3, emphasizedDecel, popin 80%"
+      "windowsOut, 1, 2, emphasizedDecel, popin 90%"
+      "windowsMove, 1, 3, emphasizedDecel, slide"
+      "border, 1, 10, emphasizedDecel"
+      # layers
+      "layersIn, 1, 2.7, emphasizedDecel, popin 93%"
+      "layersOut, 1, 2.4, menu_accel, popin 94%"
+      # fade
+      "fadeLayersIn, 1, 0.5, menu_decel"
+      "fadeLayersOut, 1, 2.7, menu_accel"
+      # workspaces
       "workspaces, 1, 7, menu_decel, slide"
-      "specialWorkspace, 1, 3, md3_decel, slidevert"
+      ## specialWorkspace
+      "specialWorkspaceIn, 1, 2.8, emphasizedDecel, slidevert"
+      "specialWorkspaceOut, 1, 1.2, emphasizedAccel, slidevert"
     ];
   };
 }
