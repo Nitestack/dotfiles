58f2387c008e459d11b3f16a34535f336c4a38d1
Nitestack <code@npham.de>
2025-07-26 04:17:30 +0200

    feat(waybar): removed modules and optimized for swaync

--- nix/modules/home/waybar/default.nix
+++ nix/modules/home/waybar/default.nix
@@ -11,20 +11,16 @@
 let
   inherit (meta) font;
 
-  blueman-manager = "${pkgs.blueman}/bin/blueman-manager";
   rofi = "${pkgs.rofi-wayland}/bin/rofi";
   hyprctl = "${osConfig.programs.hyprland.package}/bin/hyprctl";
   playerctl = "${pkgs.playerctl}/bin/playerctl";
   swaync-client = "${pkgs.swaynotificationcenter}/bin/swaync-client";
-  wpctl = "${pkgs.wireplumber}/bin/wpctl";
 
   inherit (theme.variables)
     windowBgColor
     windowFgColor
     accentBgColor
-    successColor
     warningColor
-    warningBgColor
     errorColor
     headerbarBorderColor
     ;
@@ -46,6 +42,7 @@ in
         {
           layer = "top";
           position = "top";
+          height = 36;
           # Modules
           modules-left = [
             "custom/logo"
@@ -58,12 +55,8 @@ in
           modules-right = [
             "tray"
             "privacy"
-            "bluetooth"
-            "group/group-network"
-            "wireplumber"
             "custom/swaync"
             "clock"
-            "group/group-power"
           ];
           "custom/logo" = {
             format = "<span color='#5277C3'></span>";
@@ -97,74 +90,12 @@ in
             inherit spacing;
             icon-size = 18;
           };
-          bluetooth = {
-            format-connected = "<span color='${primaryColor}'>󰂱</span>";
-            format-on = "<span color='${primaryColor}'>󰂰</span>";
-            format-off = "<span color='${warningBgColor}'>󰂯</span>";
-            format-disabled = "<span color='${errorColor}'>󰂲</span>";
-            format-no-controller = "";
-            tooltip-format-connected = "Connected Devices:\n{device_enumerate}";
-            tooltip-format-enumerate-connected = "• {device_alias}";
-            tooltip-format-on = "Ready";
-            tooltip-format-off = "Idle";
-            tooltip-format-disabled = "Disabled";
-            on-click = blueman-manager;
-          };
-          "group/group-network" = {
-            orientation = "inherit";
-            drawer.click-to-reveal = true;
-            modules = [
-              "network"
-              "network#download"
-              "network#upload"
-            ];
-          };
-          network = {
-            format-ethernet = "󰈀";
-            format-wifi = "󰖩";
-            format-disconnected = "<span color='${errorColor}'>󰖪</span>";
-            tooltip-format = "<tt>{ipaddr}</tt>";
-            tooltip-format-wifi = "<b>{essid}</b> ({frequency} GHz)\n<tt>{gwaddr}</tt>";
-            tooltip-format-disconnected = "Disconnected";
-          };
-          "network#download" = {
-            interval = 1;
-            format = "󰇚 {bandwidthDownBits}";
-            tooltip-format = "Receiving";
-          };
-          "network#upload" = {
-            interval = 1;
-            format = "󰕒 {bandwidthUpBits}";
-            tooltip-format = "Sending";
-          };
-          wireplumber = {
-            format = "{icon} {volume}%";
-            format-muted = "<span color='${errorColor}'> {volume}%</span>";
-            format-icons = [
-              ""
-              ""
-              " "
-            ];
-            on-click = "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-          };
           "custom/swaync" = {
             tooltip = false;
             exec = "${swaync-client} -swb";
-            format = "{icon}";
-            format-icons = {
-              dnd-inhibited-notification = "󰂛<span foreground='red'><sup></sup></span>";
-              dnd-notification = "󰂛<span foreground='red'><sup></sup></span>";
-              inhibited-notification = "󱏧<span foreground='red'><sup></sup></span>";
-              notification = "󰂚<span foreground='red'><sup></sup></span>";
-
-              dnd-inhibited-none = "󰂛";
-              dnd-none = "󰂛";
-              inhibited-none = "󱏧";
-              none = "󰂚";
-            };
+            format = "󰘳{icon}";
             return-type = "json";
             on-click = "${swaync-client} -t -sw";
-            on-click-right = "${swaync-client} -d -sw";
             escape = true;
           };
           clock = {
@@ -181,36 +112,6 @@ in
               on-scroll-up = "shift_down";
               on-scroll-down = "shift_up";
             };
-          };
-          "group/group-power" = {
-            orientation = "inherit";
-            drawer.transition-left-to-right = false;
-            modules = [
-              "custom/shutdown"
-              "custom/lock"
-              "custom/suspend"
-              "custom/reboot"
-            ];
-          };
-          "custom/shutdown" = {
-            tooltip = false;
-            on-click = "systemctl poweroff";
-            format = "<span color='${errorColor}'></span>";
-          };
-          "custom/lock" = {
-            tooltip = false;
-            on-click = "loginctl lock-session";
-            format = "<span color='${primaryColor}'></span>";
-          };
-          "custom/suspend" = {
-            tooltip = false;
-            on-click = "systemctl suspend";
-            format = "<span color='${warningBgColor}'></span>";
-          };
-          "custom/reboot" = {
-            tooltip = false;
-            on-click = "systemctl reboot";
-            format = "<span color='${successColor}'></span>";
           };
         }
       ];
