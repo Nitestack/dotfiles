4e94144ee806c4aa4144e9c88ceb1b85253c919b
Nitestack <code@npham.de>
2025-08-20 22:49:14 +0200

    feat(nix): add port `3000` for web development

--- configurations/nixos/nixstation/default.nix
+++ configurations/nixos/nixstation/default.nix
@@ -79,10 +79,12 @@ in
   networking = {
     networkmanager.enable = true;
     hostName = "nixstation";
-    # Spotify
     firewall = {
-      allowedTCPPorts = [ 57621 ]; # sync local tracks from fs with mobile devices in the same network
-      allowedUDPPorts = [ 5353 ]; # enables discovery of Spotify Connect devices
+      allowedTCPPorts = [
+        57621 # Spotify: sync local tracks from fs with mobile devices in the same network
+        3000 # web development
+      ];
+      allowedUDPPorts = [ 5353 ]; # Spotify: enables discovery of Spotify Connect devices
     };
   };
 }
